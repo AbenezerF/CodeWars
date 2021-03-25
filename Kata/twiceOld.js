9c4f35d793920103680e0758510546fbf7da4f96
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-03-25 16:00:51 -0400

    Add Twice as Old Kata

---
+++ Kata/twiceOld.js
@@ -0,0 +1,13 @@
+/* Twice as Old (8 kyu)
+Your function takes two arguments:
+
+1. current father's age (years)
+2. current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+*/
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let wantedAge = sonYearsOld * 2;
+    return Math.abs(dadYearsOld - wantedAge)
+  }
