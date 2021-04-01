0f5100814dc490fdea3eaff26b4b8e4a9b804c99
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-01 18:07:48 -0400

    Update templateStr.js

--- Kata/templateStr.js
+++ Kata/templateStr.js
@@ -11,6 +11,6 @@ Output
 You must output a string containing the two strings with the word ```' are '```
 */
 
-var TempleStrings = function(obj, feature) {
+var TempleStrings = function(obj,feature) {
     return `${obj} are ${feature}`
   }
