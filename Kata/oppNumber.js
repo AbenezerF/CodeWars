73275d96467aac0f595d17c04b09bcfa3b1763d3
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-03 21:20:37 -0400

    Add Opposite Number Kata

---
+++ Kata/oppNumber.js
@@ -0,0 +1,15 @@
+/* Opposite Number (8kyu)
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+    return number * -1;
+  }
+
