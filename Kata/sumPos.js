1ea9d79e1558adfbcfe578958a326cd0d0eec3fa
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-03 21:16:31 -0400

    Update sumPos.js

--- Kata/sumPos.js
+++ Kata/sumPos.js
@@ -6,5 +6,5 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 */
 
 function positiveSum(arr) {
-    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0)
  }
