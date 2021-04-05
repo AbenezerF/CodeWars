ce9aac72ad4acf51d907ca7ec78fbba093c3839b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-05 16:06:05 -0400

    Update removeStrSpaces.js

--- Kata/removeStrSpaces.js
+++ Kata/removeStrSpaces.js
@@ -3,5 +3,5 @@ Simple, remove the spaces from the string, then return the resultant string.
 */
 
 function noSpace(x){
-    return x.split(' ').join('')
-    }
+    return x.split(' ').join('');
+}
