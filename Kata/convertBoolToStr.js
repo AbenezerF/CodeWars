d92e48500a43e87424ff69c73bb1b34fc05e4f4b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-06 18:57:29 -0400

    Update convertBoolToStr.js

--- Kata/convertBoolToStr.js
+++ Kata/convertBoolToStr.js
@@ -3,5 +3,5 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 or a "No" string for false.
 */
 function boolToWord( bool ){
-    return bool ? 'Yes': 'No'
-  }
+    return bool ? 'Yes': 'No';
+}
