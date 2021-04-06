a04a25b22673719bdd51b25cc2dd9695d357c3d7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-06 19:05:37 -0400

    Add Is n Divisible by X and Y Kata

---
+++ Kata/nDivisibleXY.js
@@ -0,0 +1,8 @@
+/* Is n divisible by x and y? (8kyu)
+Create a function that checks if a number n is divisible by two numbers x AND y. 
+All inputs are positive, non-zero digits.
+*/
+
+function isDivisible(n, x, y) {
+    return (n % x === 0) && (n % y === 0)
+  }
