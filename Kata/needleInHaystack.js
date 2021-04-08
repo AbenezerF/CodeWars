756ddbf5044aed934722d94b6db3c0ec678c96e6
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-08 15:16:54 -0400

    Update needleInHaystack.js

--- Kata/needleInHaystack.js
+++ Kata/needleInHaystack.js
@@ -11,4 +11,4 @@ findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
 
 function findNeedle(haystack) {
     return "found the needle at position " + haystack.indexOf("needle");
-  }
+}
