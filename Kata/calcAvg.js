582d45978d0aa1553fec1313b019cabf9a2dc46c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-13 21:46:53 -0400

    Add Calculate Average Kata

---
+++ Kata/calcAvg.js
@@ -0,0 +1,10 @@
+/* Calculate average (8kyu)
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0
+*/
+
+function find_average(array) {
+    let sum = array.reduce((a, b) => a + b, 0);
+    return sum/array.length;
+  }
