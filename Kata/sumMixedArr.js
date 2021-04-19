b0373c0f0f285d8fc2ecce71c588bf64a82bdacd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-19 12:26:55 -0400

    Add Sum Mixed Arrays Kata

---
+++ Kata/sumMixedArr.js
@@ -0,0 +1,19 @@
+/* Sum Mixed Array (8kyu)
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+
+function sumMix(x){
+    let sum = 0;
+    for(let val of x){
+      sum += Number(val)
+    }
+    return sum;
+  }
+
+// using in-built methods
+  function sumMix(x){
+    return x.map((a) => +a).reduce((acc,curr) => acc + curr) 
+  }
