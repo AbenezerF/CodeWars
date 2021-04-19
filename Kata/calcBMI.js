178bf912c52e0ee9fcf859fd7428f239734991ba
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-19 12:36:39 -0400

    Update calcBMI.js

--- Kata/calcBMI.js
+++ Kata/calcBMI.js
@@ -12,12 +12,13 @@ if bmi > 30 return "Obese"
 
 function bmi(weight, height) {
     let bmi = weight/height ** 2;
+    
     if(bmi <= 18.5 ){
       return 'Underweight'
     }else if(bmi <= 25.0){
-      return 'Normal'
+      return 'Normal';
     }else if(bmi <= 30.0){
-      return 'Overweight'
+      return 'Overweight';
     }
-    return 'Obese'
-  }
+    return 'Obese';
+ }
