978413452d6bbf3c334485f0ea89dd92ea040866
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-19 12:27:27 -0400

    Update sumMixedArr.js

--- Kata/sumMixedArr.js
+++ Kata/sumMixedArr.js
@@ -8,12 +8,12 @@ Return your answer as a number.
 function sumMix(x){
     let sum = 0;
     for(let val of x){
-      sum += Number(val)
+      sum += Number(val);
     }
     return sum;
   }
 
 // using in-built methods
   function sumMix(x){
-    return x.map((a) => +a).reduce((acc,curr) => acc + curr) 
-  }
+    return x.map((a) => +a).reduce((acc,curr) => acc + curr);
+ }
