87a81a46616a474812ee79271c4298c3950ef1ba
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-20 13:56:03 -0400

    Update playingBanjo.js

--- Kata/playingBanjo.js
+++ Kata/playingBanjo.js
@@ -12,4 +12,4 @@ Names given are always valid strings.
 
 function areYouPlayingBanjo(name) {
     return name[0].toLowerCase() === 'r' ? `${name} plays banjo`: `${name} does not play banjo`;
-  }
+}
