77adf4b24a08b4b36e16911e07821158cbcdf61a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-22 14:17:54 -0400

    Update feastBeast.js

--- Kata/feastBeast.js
+++ Kata/feastBeast.js
@@ -12,5 +12,5 @@ will not contain numerals.
 */
 
 function feast(beast, dish) {
-    return dish[0] === beast[0] && dish[dish.length - 1] === beast[beast.length -1] ? true : false;
-   }
+   return dish[0] === beast[0] && dish[dish.length - 1] === beast[beast.length -1] ? true : false;
+}
