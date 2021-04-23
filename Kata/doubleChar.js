ab04800851f03445f71b424f63d346cc4bb43329
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-23 13:56:10 -0400

    Add Double Char Kata

---
+++ Kata/doubleChar.js
@@ -0,0 +1,14 @@
+/* Double Char (8kyu)
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+doubleChar("1234!_ ") ==> "11223344!!__  "
+
+*/
+
+function doubleChar(str) {
+    return str.split("").map(function (c) {
+      return c + c
+    }).join("")
+  }
