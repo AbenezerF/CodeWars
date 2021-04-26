886c5e690a0e093faf3f70364922bd376360c98a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-26 16:20:02 -0400

    Update divisibleNums.js

--- Kata/divisibleNums.js
+++ Kata/divisibleNums.js
@@ -6,5 +6,5 @@ divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
 */
 
 function divisibleBy(numbers, divisor){
-    return numbers.filter( (x) => x % divisor === 0)
-  }
+    return numbers.filter((x) => x % divisor === 0);
+}
