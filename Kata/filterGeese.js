18f44617af5b58b52e8c3b4026cc270ab248dbd9
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-04-29 11:19:10 -0400

    Update filterGeese.js

--- Kata/filterGeese.js
+++ Kata/filterGeese.js
@@ -17,7 +17,6 @@ and some elements may be repeated.
 */
 
 function gooseFilter (birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    
-    return birds.filter( (x) => !geese.includes(x))
-  };
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter( (x) => !geese.includes(x));
+};
