e9bf0e43a01bf46a96c8d7988f8d4973c4d1892a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-03 13:34:27 -0400

    Update checkExam.js

--- Kata/checkExam.js
+++ Kata/checkExam.js
@@ -17,11 +17,11 @@ checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 */
 
 function checkExam(array1, array2) {
-    let sum = 0;
-     for(let i = 0; i < array1.length; i++){
-       if(array1[i] === array2[i]) sum += 4;
-       else if(array2[i] === '') sum += 0;
-       else if(array1[i] !== array2[i]) sum -= 1
-     }
-      return sum < 0 ? 0 : sum;
+   let sum = 0;
+    for(let i = 0; i < array1.length; i++){
+      if(array1[i] === array2[i]) sum += 4;
+      else if(array2[i] === '') sum += 0;
+      else if(array1[i] !== array2[i]) sum -= 1;
     }
+  return sum < 0 ? 0 : sum;
+ }
