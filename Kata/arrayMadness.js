dce46b68ecfec42bdd29943f1008cef4ae4add57
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-06 18:24:32 -0400

    Update arrayMadness.js

--- Kata/arrayMadness.js
+++ Kata/arrayMadness.js
@@ -10,10 +10,10 @@ Get your timer out. Are you ready? Ready, get set, GO!!!
 
 function arrayMadness(a, b) {
     let sumA = a.map(num => num ** 2);
-      let sumB = b.map(num => num ** 3);
+    let sumB = b.map(num => num ** 3);
     
-      sumA = sumA.reduce((x, y) => x + y, 0);
-      sumB = sumB.reduce((x, y) => x + y, 0);
+    sumA = sumA.reduce((x, y) => x + y, 0);
+    sumB = sumB.reduce((x, y) => x + y, 0);
     
-      return sumA > sumB ? true : false;
-    }
+  return sumA > sumB ? true : false;
+}
