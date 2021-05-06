58c8b551ae93ce2835a3c6e471d59326d9bc9ddd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-06 18:29:21 -0400

    Update validationRegex.js

--- Kata/validationRegex.js
+++ Kata/validationRegex.js
@@ -8,5 +8,5 @@ Length should be between 4 and 16 characters (both included).
 
 */
 function validateUsr(username) {
-    return /^[0-9a-z_]{4,16}$/.test(username)
+    return /^[0-9a-z_]{4,16}$/.test(username);
   }
