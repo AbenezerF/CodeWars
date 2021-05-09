228b24fa791b36a9c6b4ab573157573078f45c25
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-09 13:26:20 -0400

    Update checkForFactor.js

--- Kata/checkForFactor.js
+++ Kata/checkForFactor.js
@@ -16,5 +16,5 @@ Note: base is a non-negative number, factor is a positive number.
 */
 
 function checkForFactor (base, factor) {
-    return base % factor === 0 ? true : false
+    return base % factor === 0;
   }
