62f6a530c6d1eb0f75246095b974d2851c363d36
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-10 14:02:33 -0400

    Add Sum the Strings Kata

---
+++ Kata/sumStrings.js
@@ -0,0 +1,13 @@
+/* Sum The Strings (8kyu)
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+*/
+
+function sumStr(a,b) {
+    let sum = Number(a) + Number(b) 
+    return sum.toString()
+  }
