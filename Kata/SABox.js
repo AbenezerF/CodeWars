3e6598fc503e0e6afa51de80894f374b738462bc
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-15 12:53:20 -0400

    Add Surface Area and Volume of a Box Kata

---
+++ Kata/SABox.js
@@ -0,0 +1,5 @@
+/* Surface Area and Volume of a Box (8kyu)
+Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+*/
+
+const getSize = (width, height, depth) => [(width * height + width * depth + height * depth) * 2, width * height * depth]
