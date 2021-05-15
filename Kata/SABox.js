dc69fe9f016e2553fb0c625c9b7f38b77a9dd9c7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-15 12:53:35 -0400

    Update SABox.js

--- Kata/SABox.js
+++ Kata/SABox.js
@@ -2,4 +2,4 @@
 Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 */
 
-const getSize = (width, height, depth) => [(width * height + width * depth + height * depth) * 2, width * height * depth]
+const getSize = (width, height, depth) => [(width * height + width * depth + height * depth) * 2, width * height * depth];
