a4daa3f7ab7ed6f830ca28b568c070b000dfb76d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-16 12:10:48 -0400

    Add Bin to Decimal Kata

---
+++ Kata/binToDecimal.js
@@ -0,0 +1,7 @@
+/* Bin to Decimal (8kyu)
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+
+function binToDec(bin){
+    return parseInt(bin,2)
+  }
