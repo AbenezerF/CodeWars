a130af585cab3c63584cb0ad8d95bded17efc7e9
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-19 18:15:12 -0400

    Update merge.js

--- Kata/merge.js
+++ Kata/merge.js
@@ -23,6 +23,6 @@ function mergeArrays(arr1, arr2) {
     return [...arr1, ...arr2].sort((a, b) => {
       return a - b;
     }).filter((el, index, arr) => {
-      return arr.indexOf(el) === index
+      return arr.indexOf(el) === index;
     });
   }
