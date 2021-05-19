fe01b7981c736a5909a537f57be36ffb251189da
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-19 18:08:59 -0400

    Update gravityFlip.js

--- Kata/gravityFlip.js
+++ Kata/gravityFlip.js
@@ -23,6 +23,6 @@ flip('L', [1, 4, 5, 3, 5])  =>  [5, 5, 4, 3, 1]
 */
 
 const flip=(d, a)=>{
-    if(d === 'R') return a.sort((a,b)=>a-b)
-    if(d === 'L') return a.sort((a,b)=>b-a)
+    if(d === 'R') return a.sort((a,b)=>a-b);
+    if(d === 'L') return a.sort((a,b)=>b-a);
   }
