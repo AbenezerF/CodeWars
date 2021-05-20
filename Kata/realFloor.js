11c6ceb453016d715c4473107600a51c1256bc88
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-20 12:46:12 -0400

    Update realFloor.js

--- Kata/realFloor.js
+++ Kata/realFloor.js
@@ -18,7 +18,7 @@ Examples
 */
 
 const getRealFloor = n => {
-    if(n >= 13) return n - 2
-    if(n > 0) return n - 1
-    return n
+    if(n >= 13) return n - 2;
+    if(n > 0) return n - 1;
+    return n;
   }
