6af88378fccfe145d3066c823fac010cbe0ba142
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-23 00:42:44 -0400

    Update messiGoal.js

--- Kata/messiGoal.js
+++ Kata/messiGoal.js
@@ -17,8 +17,8 @@ copaDelReyGoals
 Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
 */
 
-var laLigaGoals = 43
-var championsLeagueGoals = 10
-var copaDelReyGoals = 5
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
 
-var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
