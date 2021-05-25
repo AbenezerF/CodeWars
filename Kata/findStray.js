ebc440afbf4daf9d9f3d0580cb8115bc2d116a6b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-25 14:54:49 -0400

    Update findStray.js

--- Kata/findStray.js
+++ Kata/findStray.js
@@ -14,7 +14,7 @@ function stray(numbers) {
     var a = numbers.sort();
     
     if(a[0] != a[1]) {
-      return a[0]
+      return a[0];
     } 
-    return a[a.length-1]
+    return a[a.length-1];
   }
