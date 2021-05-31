e73840711d305cd808c831c00d56f041ba39c50f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-05-30 21:25:50 -0400

    Add Parse Float Kata

---
+++ Kata/parseFloat.js
@@ -0,0 +1,8 @@
+/* Parse float (8kyu)
+Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+*/
+
+function parseF(s) {
+    const f = parseFloat(s)
+    return isNaN(f) ? null : f
+  }
