a56dce60bcb0411e8d6356310f4616d979a5fc7a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-06-09 22:06:50 -0400

    Add Training JS #6: Basic data types--Boolean and conditional statements if..else Kata

--- Kata/conditional.js
+++ Kata/conditional.js
@@ -1,65 +1,27 @@
-/* Training JS #8: Conditional statement--switch (8kyu)
+/* Training JS #6: Basic data types--Boolean and conditional statements if..else (8kyu)
+In javascript, Boolean object only has two value, true or false. Use ===, ==, >, <, >=, <= between two variables(or constant), the result of such an expression is a bool value. The operation between the two bool values can be used with logical operators:&&, || or some bitwise operators:&, |, !, ^
 
+Many of the data are likely to be implicitly converted to Boolean values in a given context, most common in conditional statements. In other words, the Boolean value is mainly used for conditional judgment. Look this example:
+
+var arr=[];
+...
+...
+......
+if (arr.length) return "arr has elements";
+else            return "arr is empty";
+You can see, arr is defined as an array, arr.length should be a number value, but in this example, the number value should convert to bool value. it means, if arr.length value is 0, convert value to false, if arr.length value is not 0, convert value to true.
+
+These values are converted to false in the conditional statement:
+
+0, -0, "", null, undefined, NaN
 Task
-Complete function howManydays, function accept 1 parameters:month, means the month of year, different month has different days (refer to the following table), return a number that how many days in this month(month is always greater than 0, less than or equal to 12).
-
-+---------------+-------------+
-|    month      |    days     |
-+---------------+-------------+
-|1,3,5,7,8,10,12|     31      |
-+---------------+-------------+
-|4,6,9,11       |     30      |
-+---------------+-------------+
-|2              |     28      |  (Do not consider the leap year)
-+---------------+-------------+
-little tips: Use default for most of the cases can reduce your work.
+Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
 
 When you have finished the work, click "Run Tests" to see if your code is working properly.
 
 In the end, click "Submit" to submit your code pass this kata.
-
-
 */
 
-function howManydays(month){
-    var days;
-    switch (month){
-      case 1:
-        days = 31;
-        break;
-      case 2:
-        days = 28;
-        break;
-      case 3:
-        days = 31;
-        break;
-      case 4:
-        days = 30;
-        break;
-      case 5:
-        days = 31;
-        break;
-      case 6:
-        days = 30;
-        break;
-      case 7:
-        days = 31;
-        break;
-      case 8:
-        days = 31;
-        break;
-      case 9:
-        days = 30;
-        break;
-      case 10:
-        days = 31;
-        break;
-      case 11:
-        days = 30;
-        break;
-      case 12:
-        days = 31;
-        break;
-    }
-    return days;
+function trueOrFalse(val){
+    return val ? true : false            
   }
