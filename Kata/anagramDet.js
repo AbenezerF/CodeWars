e21ac580b5cb072eed992de86aa77ea76b6f1f0e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2021-06-20 14:17:32 -0400

    Update anagramDet.js

--- Kata/anagramDet.js
+++ Kata/anagramDet.js
@@ -14,5 +14,5 @@ Examples
 var isAnagram = function(test, original) {
     var t = test.toLowerCase().split('').sort().join('');
     var o = original.toLowerCase().split('').sort().join('');
-    return (t==o)?true:false;
+    return (t==o) ? true:false;
   };
