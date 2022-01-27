f6bfa8f8ff0902a38f1d0f9edcbffb8ab2d656c2
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-01-26 21:18:26 -0500

    Basic variable assignment Kata

---
+++ Kata/varErr.js
@@ -0,0 +1,15 @@
+/* Basic variable assignment (8kyu)
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+*/
+
+
+// error code
+var a == "code";
+var b =="wa.rs";
+var name == a + b;
+
+
+// fixed code
+var a = "code";
+var b ="wa.rs";
+var name = a + b;
