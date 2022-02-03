9e60f3e23491c67b35520ddb185ffe48483aa6f0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-02 20:20:20 -0500

    Add Basic Variable Assignment Kata

---
+++ Kata/varAssign.js
@@ -0,0 +1,14 @@
+/* Basic variable assignment (8kyu)
+This code should store "codewa.rs" as a variable called name 
+but it's not working. Can you figure out why?
+*/
+
+// previous
+// var a == "code";
+//var b == "wa.rs";
+//var name == a + b;
+
+//after corrections
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
