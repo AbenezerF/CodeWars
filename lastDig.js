20bc55d85ce3ec418322de5f3bf144e6162a4863
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-10 12:20:22 -0500

    Delete lastDig.js

--- lastDig.js
+++
@@ -1,6 +0,0 @@
-function lastDigit(n, d) {
-    let val = n.toString().split('').map( x => parseInt(x))
-    if(d >= val.length) return val
-    if(d <= 0) return []
-    return val.slice(val.length-d, val.length)
-  }
