5ffdf17a009e4729b4f9c662aa0e4d16dd789dbd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-11 11:10:03 -0500

    Add Remove the Minimum Kata

---
+++ Kata/removeMin.js
@@ -0,0 +1,17 @@
+
+
+function removeSmallest(numbers) {
+    let small = numbers[0]
+    let arr = []
+    for(let i = 0; i < numbers.length; i++){
+      if(numbers[i] < small){
+        small = numbers[i]
+      }
+    }
+    for(let i = 0; i < numbers.length; i++){
+      arr.push(numbers[i])
+    }
+    let ind = arr.findIndex(rank => rank === small)
+    let removed = arr.splice(ind, 1)
+    return arr
+}
