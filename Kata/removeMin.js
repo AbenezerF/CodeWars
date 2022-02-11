7c05e893e79ddea288d151351ea44a04a2b6cd22
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-11 11:10:33 -0500

    Update removeMin.js

--- Kata/removeMin.js
+++ Kata/removeMin.js
@@ -1,17 +1,17 @@
 
 
 function removeSmallest(numbers) {
-    let small = numbers[0]
-    let arr = []
+    let small = numbers[0];
+    let arr = [];
     for(let i = 0; i < numbers.length; i++){
       if(numbers[i] < small){
-        small = numbers[i]
+        small = numbers[i];
       }
     }
     for(let i = 0; i < numbers.length; i++){
-      arr.push(numbers[i])
+      arr.push(numbers[i]);
     }
-    let ind = arr.findIndex(rank => rank === small)
-    let removed = arr.splice(ind, 1)
-    return arr
+    let ind = arr.findIndex(rank => rank === small);
+    let removed = arr.splice(ind, 1);
+    return arr;
 }
