3dd6524f636f341690129ed82507e0912007f05a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-11 10:47:27 -0500

    Update oddEven.js

--- Kata/oddEven.js
+++ Kata/oddEven.js
@@ -2,7 +2,7 @@
 
 
 function oddOrEven(array) {
-    if(array.length === 0) array.push(0)
-    let val = array.reduce( (prev,curr) => prev + curr) 
-    return val % 2 === 0 ? 'even' : 'odd'
+    if(array.length === 0) array.push(0);
+    let val = array.reduce( (prev,curr) => prev + curr);
+    return val % 2 === 0 ? 'even' : 'odd';
   }
