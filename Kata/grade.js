038d6c27c4847d57f59a0e51dcc89e0c52fee69b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-12 13:29:26 -0500

    Update grade.js

--- Kata/grade.js
+++ Kata/grade.js
@@ -13,17 +13,17 @@ Tested values are all between 0 and 100. Theres is no need to check for negative
 */
 
 function getGrade (s1, s2, s3) {
-    let avg = (s1 + s2 +s3) / 3
+    let avg = (s1 + s2 +s3) / 3;
     switch(true){
         case (avg >= 90):
-           return 'A'
+           return 'A';
         case (avg >= 80):
-            return 'B'
+            return 'B';
         case (avg >= 70):
-          return 'C'
+          return 'C';
         case (avg >= 60):
-          return 'D'
+          return 'D';
         default:
-          return 'F'
+          return 'F';
     }
   }
