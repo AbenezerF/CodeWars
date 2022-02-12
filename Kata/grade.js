2e7efbf00919971a7ecef87db0e271e1a868b5d3
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-12 13:28:48 -0500

    Update grade.js

--- Kata/grade.js
+++ Kata/grade.js
@@ -11,3 +11,19 @@ Numerical Score	Letter Grade
 0 <= score < 60	'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 */
+
+function getGrade (s1, s2, s3) {
+    let avg = (s1 + s2 +s3) / 3
+    switch(true){
+        case (avg >= 90):
+           return 'A';
+        case (avg >= 80):
+            return 'B';
+        case (avg >= 70):
+          return 'C';
+        case (avg >= 60):
+          return 'D';
+        default:
+          return 'F';
+    }
+  }
