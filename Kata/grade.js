34cfefbf3081a78e14d0e07f9e998b2d7ff28574
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-12 13:30:09 -0500

    Update grade.js

--- Kata/grade.js
+++ Kata/grade.js
@@ -2,6 +2,7 @@
 /*
 
 Grade book
+
 Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 
 Numerical Score	Letter Grade
