852931e2974795913a7738bb4f9538eb8f8a52e6
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-14 17:48:51 -0500

    Update GrasshopperMessiGoals.js

--- Kata/GrasshopperMessiGoals.js
+++ Kata/GrasshopperMessiGoals.js
@@ -21,6 +21,6 @@ Create a fourth variable named totalGoals that stores the sum of all of Messi's 
 
 var laLigaGoals = 43;
 var championsLeagueGoals = 10;
-var copaDelReyGoals = 5;
+var copaDelReyGoals = 5
 
-var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
