e1c2e426775805642f14c7bc39807b04318d0f17
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-17 09:40:55 -0500

    Update sum.js

--- Kata/sum.js
+++ Kata/sum.js
@@ -1,4 +1,4 @@
-
+// Sum of Two Lowest Integers (7kyu)
 /*
 Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
 
