2b3b4ac1b6d1f15a5d770e824e9b6db3bf8d6622
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-21 14:09:47 -0500

    Update factorial.js

--- Kata/factorial.js
+++ Kata/factorial.js
@@ -10,3 +10,12 @@ or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or
 IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError 
 (JavaScript) or ValueError (Python) or return -1 (C).
 */
+
+function factorial(n){
+  let output = 0
+  if(n < 0 || n > 12){
+    throw new RangeError("The argument must be between 0 and 12.")
+  }
+  if(n === 0 || n ===1) return 1;
+  return (n * factorial(n-1))
+}
