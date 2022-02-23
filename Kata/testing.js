5f2e51034348245fe85f2b735a610e879e014ce0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-23 12:19:31 -0500

    Update testing.js

--- Kata/testing.js
+++ Kata/testing.js
@@ -12,3 +12,11 @@ Examples:
 number([]) // => []
 number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
 */
+
+
+var number=function(array){
+  for(let i = 0; i < array.length; i++){
+    array[i] = i +1 + ": " + array[i]
+  }
+  return array
+}
