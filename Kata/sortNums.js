febb86e092a026a4e87a370ba60fc045a019ba40
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-23 12:14:51 -0500

    Update sortNums.js

--- Kata/sortNums.js
+++ Kata/sortNums.js
@@ -10,5 +10,5 @@ solution(null); // should return []
 */
 
 function solution(nums){
-  return nums !== null ? nums.sort((a,b) => a-b) : []
+  return nums !== null ? nums.sort((a,b) => a-b) : [];
 }
