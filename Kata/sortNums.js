172c94fcf9c7edcc56050a8989aa0dcadf5d2d80
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-23 12:14:16 -0500

    Update sortNums.js

--- Kata/sortNums.js
+++ Kata/sortNums.js
@@ -1,4 +1,4 @@
-
+// Sort Numbers (7kyu)
 
 /*
 Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
