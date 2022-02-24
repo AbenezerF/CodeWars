065be889dada02195e05f426a9e6f6fc6bd66fc2
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-24 13:31:30 -0500

    Update findCapitals.js

--- Kata/findCapitals.js
+++ Kata/findCapitals.js
@@ -1,4 +1,5 @@
 // Find the Capitals (7Kyu)
+
 /*
 Instructions
 Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
