c9a45d69cc6e1e9509d2416bee742ef98cbfee44
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-24 13:30:52 -0500

    Update findCapitals.js

--- Kata/findCapitals.js
+++ Kata/findCapitals.js
@@ -6,3 +6,13 @@ Write a function that takes a single string (word) as argument. The function mus
 Example
 Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
+
+var capitals = function (word) {
+  let arr = []
+	for(let i = 0; i < word.length; i++){
+      if(word.charAt(i) === word.charAt(i).toUpperCase()){
+        arr.push(i)
+      }
+  }
+  return arr
+};
