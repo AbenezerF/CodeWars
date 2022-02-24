c48dace81d92082912c452cbe9be3c3d98672680
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-24 13:30:59 -0500

    Update findCapitals.js

--- Kata/findCapitals.js
+++ Kata/findCapitals.js
@@ -8,7 +8,7 @@ Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
 
 var capitals = function (word) {
-  let arr = []
+  let arr = [];
 	for(let i = 0; i < word.length; i++){
       if(word.charAt(i) === word.charAt(i).toUpperCase()){
         arr.push(i)
