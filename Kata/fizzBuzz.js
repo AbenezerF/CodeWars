f2d7fc6f96644ec67a53dc537bce6ead798e4355
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-25 16:44:10 -0500

    Update fizzBuzz.js

--- Kata/fizzBuzz.js
+++ Kata/fizzBuzz.js
@@ -13,3 +13,19 @@ Method calling example:
 
 fizzbuzz(3) -->  [1, 2, "Fizz"]
 */
+
+function fizzbuzz(n){
+  let arr = []
+  for(let i = 1; i <= n; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      arr.push('FizzBuzz')
+    }else if(i % 3 === 0){
+      arr.push('Fizz')
+    }else if(i % 5 === 0){
+      arr.push('Buzz')
+    }else{
+      arr.push(i)
+    }
+  }
+  return arr;
+}
