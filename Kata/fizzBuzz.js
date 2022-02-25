40e67a0216d0f93a8e4388193c127906ef69597f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-25 16:44:54 -0500

    Update fizzBuzz.js

--- Kata/fizzBuzz.js
+++ Kata/fizzBuzz.js
@@ -20,7 +20,7 @@ function fizzbuzz(n){
     if(i % 3 === 0 && i % 5 === 0){
       arr.push('FizzBuzz');
     }else if(i % 3 === 0){
-      arr.push('Fizz')
+      arr.push('Fizz');
     }else if(i % 5 === 0){
       arr.push('Buzz')
     }else{
