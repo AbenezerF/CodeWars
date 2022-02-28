71f4811632654a6d1438431749a2f6376382423c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-02-28 15:45:19 -0500

    Update sumCubes.js

--- Kata/sumCubes.js
+++ Kata/sumCubes.js
@@ -15,7 +15,7 @@ Examples: (Input --> output)
 function sumCubes(n){
   let total = 0;
   for(let i = 1; i <= n; i++){
-    total += Math.pow(i,3)
+    total += Math.pow(i,3);
   }
   return total
 }
