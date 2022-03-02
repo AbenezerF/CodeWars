e6a65a10e8cd2ce8c37d509bc0b7a605f7a7695f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-02 10:02:29 -0500

    Update beadCount.js

--- Kata/beadCount.js
+++ Kata/beadCount.js
@@ -1,4 +1,4 @@
-
+// Simple Beads Count Kata
 /*
 Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
 
