46b983dda34074b1a0a3587a65a6eb3b53c813c7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-05 09:04:10 -0500

    Update smallestVal.js

--- Kata/smallestVal.js
+++ Kata/smallestVal.js
@@ -1,4 +1,4 @@
-
+// Smallest Value in Array (7Kyu)
 
 /*
 Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
