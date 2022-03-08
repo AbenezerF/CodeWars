a3a711ef64ee76e146cf4918519984325e4b440a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-08 12:50:52 -0500

    Update evenArr.js

--- Kata/evenArr.js
+++ Kata/evenArr.js
@@ -1,4 +1,4 @@
-
+// Even numbers in an array (7Kyu)
 
 /*
 Given an array of digitals numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
