c73fadde46b8ea88cbe891757d255caf1ace6e8b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-08 12:51:33 -0500

    Update evenArr.js

--- Kata/evenArr.js
+++ Kata/evenArr.js
@@ -17,7 +17,7 @@ function evenNumbers(array, number) {
   array = array.reverse();
   for(let i = 0; i < array.length; i++){
     if(array[i] % 2 === 0 && final.length < number){
-      final.push(array[i])
+      final.push(array[i]);
     }
   }
   return final.reverse()
