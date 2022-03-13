2c791a3c2d1766277e4c4489a775426b6e53b97e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-13 15:05:33 -0400

    Update reverseWords.js

--- Kata/reverseWords.js
+++ Kata/reverseWords.js
@@ -1,4 +1,4 @@
-
+// Reversing Words in a String (7kyu)
 
 /*
 You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
