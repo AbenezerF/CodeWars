e06675b5983cf66498f410f435fcb8801b2fdb72
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-14 14:47:47 -0400

    Update convertStrNum.js

--- Kata/convertStrNum.js
+++ Kata/convertStrNum.js
@@ -11,6 +11,7 @@ Create the function that takes as a parameter a sequence of numbers represented 
 ie:["1", "2", "3"] to [1, 2, 3]
 
 Note that you can receive floats as well.
+
 */
 
 function toNumberArray(stringarray){
