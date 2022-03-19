aaf53d2a5534d94ef181fa73279a967ed14916c4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-19 12:52:43 -0400

    Update palindrome.js

--- Kata/palindrome.js
+++ Kata/palindrome.js
@@ -3,3 +3,8 @@
 /*
 Write a function that checks if a given string (case insensitive) is a palindrome.
 */
+
+
+function isPalindrome(x) {
+  return x.toLowerCase() === x.split('').reverse('').join('').toLowerCase()
+}
