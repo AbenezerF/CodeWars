235e2128e152d7b4eb0d093edae3af05223813fc
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-23 10:23:14 -0400

    Update divConq.js

--- Kata/divConq.js
+++ Kata/divConq.js
@@ -11,7 +11,7 @@ function divCon(x){
   let ints = x.filter( y => typeof y === 'number');
   let strings = x.filter( y => typeof y === 'string');
   
-  let totInts = ints.reduce( (acc, curr) => acc += curr, 0)
+  let totInts = ints.reduce( (acc, curr) => acc += curr, 0);
   let totString = strings.reduce( (acc, curr) => acc += +curr, 0)
   
  return totInts - totString
