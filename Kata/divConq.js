47d662799f8b42c32b0b39f0473ac778f59c3a60
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-23 10:22:15 -0400

    Update divConq.js

--- Kata/divConq.js
+++ Kata/divConq.js
@@ -1,4 +1,4 @@
-
+// Divide and Conquer (7Kyu)
 
 /*
 Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
