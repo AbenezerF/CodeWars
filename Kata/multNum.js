ee971e786da082e2cb645292383f5ca31dc7913b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-25 10:56:46 -0400

    Update multNum.js

--- Kata/multNum.js
+++ Kata/multNum.js
@@ -1,4 +1,4 @@
-
+// Multiply the Number (8Kyu)
 
 /*
 Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
