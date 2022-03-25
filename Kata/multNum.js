166add7bcedb69166cb3211928c420662194e148
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-25 10:56:09 -0400

    Add Multiply the number Kata

--- Kata/multNum.js
+++ Kata/multNum.js
@@ -1,27 +1,11 @@
-/* Multiplication table for number (8kyu)
-Your goal is to return multiplication table for number that is always an integer from 1 to 10.
 
-For example, a multiplication table (string) for number == 5 looks like below:
 
-1 * 5 = 5
-2 * 5 = 10
-3 * 5 = 15
-4 * 5 = 20
-5 * 5 = 25
-6 * 5 = 30
-7 * 5 = 35
-8 * 5 = 40
-9 * 5 = 45
-10 * 5 = 50
-P. S. You can use \n in string to jump to the next line.
+/*
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
 
-Note: newlines should be added between rows, but there should be no trailing newline at the end.
+multiply(3)==15 // 3 * 5¹
+multiply(10)==250 // 10 * 5²
+multiply(200)==25000 // 200 * 5³
+multiply(0)==0 // 0 * 5¹
+multiply(-3)==-15 // -3 * 5¹
 */
-
-function multiTable(number) {
-    let table = '';
-    for(let i = 1; i <= 10; i++){
-      table += `${i} * ${number} = ${i*number}\n`;
-    }
-    return table.slice(0, -1);
-  }
