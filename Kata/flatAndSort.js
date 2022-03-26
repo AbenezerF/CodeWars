7353f22ea5a132b85b58d5a33d7bde6d439892bd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-26 09:09:56 -0400

    Update flatAndSort.js

--- Kata/flatAndSort.js
+++ Kata/flatAndSort.js
@@ -8,3 +8,9 @@ Example:
 
 Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 */
+
+"use strict";
+function flattenAndSort(array) {
+  let final = [].concat(...array)
+  return final.sort((a,b) => a -b)
+}
