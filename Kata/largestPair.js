3dae51e0b4cb0440f632b57c187f0a7d1ba291c5
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-27 13:27:01 -0400

    Update largestPair.js

--- Kata/largestPair.js
+++ Kata/largestPair.js
@@ -8,3 +8,8 @@ For example
 [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 */
+
+function largestPairSum (numbers) {
+  let final = numbers.sort( (a,b) => a -b)
+  return final[final.length-2] + final[final.length-1]
+}
