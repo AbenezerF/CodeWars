be2ed62f6a466f3973236edd11e78253e1b51981
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-28 09:39:12 -0400

    Update twoFighters.js

--- Kata/twoFighters.js
+++ Kata/twoFighters.js
@@ -30,7 +30,7 @@ function Fighter(name, health, damagePerAttack) {
 function declareWinner(fighter1, fighter2, firstAttacker) {
     while (fighter1.health > 0 && fighter2.health > 0) {
       fighter2.health -= fighter1.damagePerAttack
-      fighter1.health -= fighter2.damagePerAttack;
+      fighter1.health -= fighter2.damagePerAttack
     }
     
     if (fighter1.health <= 0 && fighter2.health <= 0)
