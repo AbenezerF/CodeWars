56e7d6a4ff14f9fb26c1bdcb8704447fbdcf3047
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-29 10:14:24 -0400

    Update thinkfulDrill.js

--- Kata/thinkfulDrill.js
+++ Kata/thinkfulDrill.js
@@ -6,3 +6,7 @@ Write a function named repeater() that takes two arguments (a string and a numbe
 Example: (Input1, Input2 --> Output)
 "a", 5 --> "aaaaa"
 */
+
+function repeater(string, n){
+  return string.repeat(n)
+}
