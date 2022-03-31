f783110889850a475bcba502c86f0f2b6d44a751
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-03-31 12:25:42 -0400

    Update numInStr.js

--- Kata/numInStr.js
+++ Kata/numInStr.js
@@ -1,4 +1,4 @@
-
+// Numbers in strings (7Kyu)
 
 /*
 In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
