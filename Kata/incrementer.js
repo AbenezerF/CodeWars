682b388096a357f439654a68330ea7ab72c2cba3
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-01 10:13:04 -0400

    Update incrementer.js

--- Kata/incrementer.js
+++ Kata/incrementer.js
@@ -14,3 +14,17 @@ Examples:
 [4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
                                        #  9+3 = 12  -->  2
 */
+
+
+function incrementer(nums) { 
+  for(let i = 0; i < nums.length; i++){
+    if( nums[i] + i + 1 === 10){
+      nums[i] = 0
+    }else if(nums[i] + i + 1 > 10){
+      nums[i] = (nums[i] + i + 1) % 10
+    }else{
+      nums[i] += (i + 1)
+    }
+  }
+  return nums
+}
