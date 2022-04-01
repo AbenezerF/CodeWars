66e725f40eb793c80f1731f6326025221b59959c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-01 10:12:47 -0400

    Update incrementer.js

--- Kata/incrementer.js
+++ Kata/incrementer.js
@@ -1,4 +1,4 @@
-
+// Incrementer (7Kyu)
 
 /*
 Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
