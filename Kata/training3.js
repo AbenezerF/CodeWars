3da442ef5f3e8084b9d0e6bb43543905c91de820
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-05 10:07:57 -0400

    Update training3.js

--- Kata/training3.js
+++ Kata/training3.js
@@ -17,7 +17,7 @@ If everything is right, click "Submit" again to submit your code pass this kata.
 
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
-  return d1 + a2 + d2
+  return d1 + a2 + d2;
 }
 function Bee(){
   return b1+e2+e2
