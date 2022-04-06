8c97ab1fc8026b6aeacd89edd596f7a6511fecdc
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-06 10:27:20 -0400

    Update boiledEggs.js

--- Kata/boiledEggs.js
+++ Kata/boiledEggs.js
@@ -1,7 +1,7 @@
-
+// Boiled Eggs (7kyu)
 
 /*
-ou are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
+You are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
 
 Your Task
 Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
