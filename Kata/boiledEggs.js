2e7071fc29088527ee4869437debbedbc4572171
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-06 10:27:52 -0400

    Update boiledEggs.js

--- Kata/boiledEggs.js
+++ Kata/boiledEggs.js
@@ -20,7 +20,7 @@ Example (Input --> Output)
 
 function cookingTime(eggs) {
   let val = eggs / 8;
-  if(eggs === 0) return 0
+  if(eggs === 0) return 0;
   else if(eggs <= 8) return 5
   else return Math.ceil(val) * 5
 }
