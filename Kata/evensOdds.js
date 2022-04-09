8cc3262f6bdfb71d24f83c6c4849fd3ba2aeee61
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-09 17:16:21 -0400

    Update evensOdds.js

--- Kata/evensOdds.js
+++ Kata/evensOdds.js
@@ -6,3 +6,7 @@ If a number is even, convert it to binary.
 If a number is odd, convert it to hex.
 Numbers will be positive. The hexadecimal string should be lowercased.
 */
+
+function evensAndOdds(num){
+	return num % 2 === 0 ? num.toString(2) : num.toString(16)
+}
