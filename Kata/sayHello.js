a8ad455b645456c10c934a3eb31ba3377965856a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-12 13:21:56 -0400

    Update sayHello.js

--- Kata/sayHello.js
+++ Kata/sayHello.js
@@ -3,7 +3,8 @@
 /*
 Say hello!
 
-Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is 
+empty string or null/nil/None.
 
 Example:
 
