7f17c864f1356691956079082f30919405028e87
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-16 14:33:07 -0400

    Update minMax.js

--- Kata/minMax.js
+++ Kata/minMax.js
@@ -4,3 +4,9 @@
 Implement a function that returns the minimal and the maximal value of a list (in this order).
 
 */
+
+function getMinMax(arr){
+  let max = Math.min(...arr)
+  let min = Math.max(...arr)
+  return [].concat(max,min)
+};
