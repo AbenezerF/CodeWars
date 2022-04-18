9655db882ac9b1f5232e637eccb4d831b84ee7a0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-18 14:34:37 -0400

    Update exten.js

--- Kata/exten.js
+++ Kata/exten.js
@@ -7,3 +7,10 @@ Task
 Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
 The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
 */
+
+
+class Cat extends Animal {
+  speak(name){
+    return this.name + " meows."
+  }
+}
