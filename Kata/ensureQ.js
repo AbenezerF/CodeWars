7c1b3f69b1b3e2dff2dc82a4fea7cf5a4ef773b5
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-21 10:38:08 -0400

    Update ensureQ.js

--- Kata/ensureQ.js
+++ Kata/ensureQ.js
@@ -1,4 +1,4 @@
-
+// Ensure Question (8kyu)
 
 /*
 Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
