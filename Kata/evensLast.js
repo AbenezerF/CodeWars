5fb49dcc34ad1446c74efc62045e9b0ad13e388e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-22 11:29:07 -0400

    Update evensLast.js

--- Kata/evensLast.js
+++ Kata/evensLast.js
@@ -12,7 +12,7 @@ function evenLast(numbers) {
   if (numbers.length === 0) return 0;
   let sum = 0;
   for (let i = 0; i < numbers.length; i += 2) {
-    sum += numbers[i]
+    sum += numbers[i];
   }
   return sum * numbers[numbers.length - 1]
 }
