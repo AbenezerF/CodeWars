7eefd8329e3ed7a79abc75b6967f8bd961044e13
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-24 23:20:41 -0400

    Update listArray.js

--- Kata/listArray.js
+++ Kata/listArray.js
@@ -14,10 +14,10 @@ Assume all inputs are valid lists with at least one value. For the purpose of si
 
 
 function listToArray(list) {
-    var temp = [];
+    var temp = []
     while(list){
-      temp.push(list.value);
-      list = list.next;
+      temp.push(list.value)
+      list = list.next
     }
-    return temp;
+    return temp
   }
