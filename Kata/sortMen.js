4c46cce1086e9cb429241b232a71b6548b5dcec4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-26 10:52:30 -0400

    Update sortMen.js

--- Kata/sortMen.js
+++ Kata/sortMen.js
@@ -40,7 +40,7 @@ Since , (Duplications are not included when separating) , then you can see only 
 function menFromBoys(arr){
     let evens = arr.filter( x => x % 2 === 0).sort( (a,b) =>  a- b);
     let odds = arr.filter( x => x % 2 !== 0).sort((a,b) =>  b- a)
-    let arrs = []
+    let arrs = [];
     evens.forEach(x => {
       if(!arrs.includes(x)) arrs.push(x);
     })
