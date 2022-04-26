7a17894daa87bb75681c0b4fb2cd9ded14666833
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-04-26 10:52:22 -0400

    Update sortMen.js

--- Kata/sortMen.js
+++ Kata/sortMen.js
@@ -42,7 +42,7 @@ function menFromBoys(arr){
     let odds = arr.filter( x => x % 2 !== 0).sort((a,b) =>  b- a)
     let arrs = []
     evens.forEach(x => {
-      if(!arrs.includes(x)) arrs.push(x)
+      if(!arrs.includes(x)) arrs.push(x);
     })
     odds.forEach(x => {
       if(!arrs.includes(x)) arrs.push(x)
