abe1872c810858a63ddd907982b6f9eda23a7b83
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-05-03 10:25:46 -0400

    Update strCase.js

--- Kata/strCase.js
+++ Kata/strCase.js
@@ -1,4 +1,4 @@
-
+// Fix string case (7kyu)
 
 /*
 In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
