c230d8cc0eef097f01d4c37a89775a67bff1f793
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-15 20:59:23 -0400

    Update ageRange.js

--- Kata/ageRange.js
+++ Kata/ageRange.js
@@ -21,3 +21,16 @@ age = 5    =>   4-5
 age = 17   =>   15-20
 
 */
+
+function datingRange(age){
+  let min = 0
+  let max = 0
+  if(age > 14){
+    min = age/2 + 7
+    max = (age-7) * 2
+  }else{
+    min =  age - 0.10 * age
+    max =  age + 0.10 * age
+  }
+  return `${Math.floor(min)}-${Math.floor(max)}`
+}
