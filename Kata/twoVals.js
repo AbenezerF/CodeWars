1dcd5e6c8982c2c890d6cfa25f2a7bb44fdd8338
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-16 12:34:30 -0400

    Update twoVals.js

--- Kata/twoVals.js
+++ Kata/twoVals.js
@@ -9,3 +9,12 @@ Examples
 0, "one", "two"    -->  []
 Good luck!
 */
+
+function alternate(n, firstValue, secondValue){
+  let arr = []
+  for(let i =0; i < n; i++){
+    if(i%2===0) arr.push(firstValue)
+    else arr.push(secondValue)
+  }
+  return arr
+}
