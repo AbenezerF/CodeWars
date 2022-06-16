092c133ec91ce5e339e43ee38183e973e8394f86
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-16 12:34:52 -0400

    Update twoVals.js

--- Kata/twoVals.js
+++ Kata/twoVals.js
@@ -14,7 +14,7 @@ function alternate(n, firstValue, secondValue){
   let arr = [];
   for(let i =0; i < n; i++){
     if(i%2===0) arr.push(firstValue);
-    else arr.push(secondValue)
+    else arr.push(secondValue);
   }
   return arr
 }
