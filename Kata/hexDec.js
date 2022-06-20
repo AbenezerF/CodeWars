d1acfd144c3533153a9acce738325c06e5051e32
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-20 10:42:14 -0400

    Update hexDec.js

--- Kata/hexDec.js
+++ Kata/hexDec.js
@@ -5,5 +5,5 @@ Complete the function which converts hex number (given as a string) to a decimal
 */
 
 function hexToDec(hexString){
- return Number.parseInt(hexString, 16);
+ return Number.parseInt(hexString, 16)
 }
