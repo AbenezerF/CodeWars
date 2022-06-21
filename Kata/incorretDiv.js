5efe903a518fc3a21b3dba629b85055567e897bb
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-21 10:33:34 -0400

    Update incorretDiv.js

--- Kata/incorretDiv.js
+++ Kata/incorretDiv.js
@@ -1,4 +1,4 @@
-
+// Incorrect division method (8kyu)
 
 /*
 This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
