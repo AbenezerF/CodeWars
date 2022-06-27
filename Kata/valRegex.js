d2fee3a97c6b51f4b4e3104d3e4beeacdf09f733
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-06-27 14:16:35 -0400

    Update valRegex.js

--- Kata/valRegex.js
+++ Kata/valRegex.js
@@ -1,3 +1,4 @@
+// validate code with simple regex (8kyu)
 
 /*
 Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
