2074cecff1bc43e5c4892dbb3be7292695e3ad27
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-01 11:11:17 -0400

    Update countChar.js

--- Kata/countChar.js
+++ Kata/countChar.js
@@ -1,4 +1,4 @@
-
+// Count the Characters (7Kyu)
 
 /*
 The goal of this kata is to write a function that takes two inputs: a string and a character. The function will count the number of times that character appears in the string. The count is case insensitive.
