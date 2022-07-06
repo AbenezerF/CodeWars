8be81e510cfcea5699df0ba70846bdb5d49610a3
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-06 12:12:22 -0400

    Update arrowFun.js

--- Kata/arrowFun.js
+++ Kata/arrowFun.js
@@ -9,5 +9,5 @@ Time to test your basic knowledge in functions! Return the odds from a list:
 */
 
 function odds(values){
-  return values.filter(x => x % 2 !== 0);
+  return values.filter(x => x % 2 !== 0)
 }
