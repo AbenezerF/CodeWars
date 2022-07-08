dabf877f2768570fda38851408f15b160573884d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-08 14:36:29 -0400

    Update debugSum.js

--- Kata/debugSum.js
+++ Kata/debugSum.js
@@ -13,7 +13,7 @@ function getSumOfDigits(integer) {
   var sum = null;
   var digits =  Math.floor(integer).toString();
   for(var ix = 0; ix < digits.length; ix++) {
-    sum += +digits[ix]
+    sum += +digits[ix];
   }
   return sum
 }
