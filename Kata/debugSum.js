8dc0fbf7d8734c20ada9a1778a85c3194913c422
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-08 14:35:24 -0400

    Update debugSum.js

--- Kata/debugSum.js
+++ Kata/debugSum.js
@@ -1,5 +1,10 @@
 //Debug Sum of Digits of a Number (7Kyu)
 
 /*
+Debug   function getSumOfDigits that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
 
+Example
+123  => 6
+223  => 7
+1337 => 14
 */
