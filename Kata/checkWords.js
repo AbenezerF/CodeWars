9db5ae51d10d430732ce5345398a98d473048c38
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-11 08:12:54 -0400

    Update checkWords.js

--- Kata/checkWords.js
+++ Kata/checkWords.js
@@ -9,3 +9,13 @@ Examples
 ["a", "a", "a", "a", "a"] ==> false // 5x "a"
 
 */
+
+
+function checkThreeAndTwo(array) {
+  let a = array.filter( x => x === 'a').length
+  let b = array.filter( x => x === 'b').length
+  let c = array.filter( x => x === 'c').length
+  
+  return (a === 3 || b === 3 || c=== 3)  
+          && (a === 2 || b === 2 || c === 2);
+}
