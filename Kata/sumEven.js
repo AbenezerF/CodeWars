4cb533cf0ffa8609e74ca4a2b690573de0f22f4d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-12 11:47:31 -0400

    Update sumEven.js

--- Kata/sumEven.js
+++ Kata/sumEven.js
@@ -1,5 +1,4 @@
 // Sum even numbers (7Kyu)
-
 /*
 Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
 
