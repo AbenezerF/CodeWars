680c610d2b8bc3151443a338be51e9d6b37f4832
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-19 08:40:02 -0400

    Update sumDiff.js

--- Kata/sumDiff.js
+++ Kata/sumDiff.js
@@ -12,3 +12,12 @@ Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 */
+
+function sumOfDifferences(arr) {
+  let total = 0
+  let sorted = arr.sort( (a,b) => b - a)
+  for(let i = 0; i < sorted.length - 1; i++){
+    total += (sorted[i] - sorted[i+1])
+  }
+  return total
+}
