f8b0d05a53b7a7456424b083528fafb878a35258
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-19 08:40:49 -0400

    Update sumDiff.js

--- Kata/sumDiff.js
+++ Kata/sumDiff.js
@@ -14,7 +14,7 @@ If the array is empty or the array has only one element the result should be 0 (
 */
 
 function sumOfDifferences(arr) {
-  let total = 0
+  let total = 0;
   let sorted = arr.sort( (a,b) => b - a)
   for(let i = 0; i < sorted.length - 1; i++){
     total += (sorted[i] - sorted[i+1])
