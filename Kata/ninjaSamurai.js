3af328c070c169f762696831cfd8f3294b26813c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-20 08:23:00 -0400

    Update ninjaSamurai.js

--- Kata/ninjaSamurai.js
+++ Kata/ninjaSamurai.js
@@ -16,6 +16,6 @@ var Warrior = function(name){
   this.health = 100
 
 this.strike = function(enemy, swings){
-  enemy.health = Math.max(0, enemy.health - (swings * 10));   
+  enemy.health = Math.max(0, enemy.health - (swings * 10))
 }
   }
