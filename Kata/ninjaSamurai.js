dab788d5df6ae0e112766936b03940b889d8d678
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-20 08:23:14 -0400

    Update ninjaSamurai.js

--- Kata/ninjaSamurai.js
+++ Kata/ninjaSamurai.js
@@ -13,7 +13,7 @@ Can you figure out what is wrong?
 
 var Warrior = function(name){
   this.name = name;
-  this.health = 100
+  this.health = 100;
 
 this.strike = function(enemy, swings){
   enemy.health = Math.max(0, enemy.health - (swings * 10))
