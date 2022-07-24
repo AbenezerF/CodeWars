b3c3ebb3bd4496dca9a10206c6a1cb2c49c2956d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-24 15:31:53 -0400

    Update isReallyNaN.js

--- Kata/isReallyNaN.js
+++ Kata/isReallyNaN.js
@@ -1,5 +1,4 @@
 // isReallyNaN (8Kyu)
-
 /*
 I've hit a few bugs in my Java/Type/Coffee-script code recently, and I've traced the problem back to the global isNaN function I was using. I had expected it to be more discerning, but it's returning true for undefined right now.
 
