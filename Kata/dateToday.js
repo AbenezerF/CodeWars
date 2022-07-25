491bebbafdff66ab5399d227a50e3c4ba69ffb83
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-25 12:09:11 -0400

    Update dateToday.js

--- Kata/dateToday.js
+++ Kata/dateToday.js
@@ -7,5 +7,5 @@ Make sure that your function does not return a false positive by only checking t
 */
 
 function isToday(date) {
-  return new Date().toDateString() === date.toDateString()
+  return new Date().toDateString() === date.toDateString();
 }
