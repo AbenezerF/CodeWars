a3883672fee550e861b6f55b4a35a3e059e9326a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-07-26 07:34:49 -0400

    Update combat.js

--- Kata/combat.js
+++ Kata/combat.js
@@ -1,5 +1,4 @@
 // Grasshopper - Terminal game combat function (8Kyu)
-
 /*
 Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. 
 Health can't be less than 0.
