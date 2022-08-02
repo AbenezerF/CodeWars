dc1efbcf0cc68af7dd5b9bc05314d61991f7cfa2
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-02 07:41:58 -0400

    Update switcheroo.js

--- Kata/switcheroo.js
+++ Kata/switcheroo.js
@@ -1,5 +1,4 @@
 // Switcheroo (7Kyu)
-
 /*
 Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
 
