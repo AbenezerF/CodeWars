3cc8def444331c829c0103fc35f52f1b57e22508
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-02 07:40:30 -0400

    Update switcheroo.js

--- Kata/switcheroo.js
+++ Kata/switcheroo.js
@@ -1,1 +1,17 @@
 // Switcheroo (7Kyu)
+
+/*
+
+*/
+
+function switcheroo(x){
+  let arr = x.split('');
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i] === 'a'){
+      arr[i] = 'b'
+    }else if(arr[i] === 'b'){
+      arr[i] = 'a'
+    }
+    }
+  return arr.join('')
+}
