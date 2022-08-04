810c007c06542f4d74784a845e022d210e90364a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-04 09:03:57 -0400

    Update outed.js

--- Kata/outed.js
+++ Kata/outed.js
@@ -13,18 +13,18 @@ Note that your boss is in the room (boss), their score is worth double it's face
 */
 
 function outed(meet, boss){
-    let sum=0;
-    let count=0;
+    let sum=0
+    let count=0
     for (var key in meet)
       if (key!=boss)
       {
-        sum+=meet[key];
-        count++;
+        sum+=meet[key]
+        count++
       }  
       else
       {
-        sum+=meet[key]*2;
-        count++;
+        sum+=meet[key]*2
+        count++
       }
-    return sum/count<=5?'Get Out Now!':'Nice Work Champ!';
+    return sum/count<=5?'Get Out Now!':'Nice Work Champ!'
   }
