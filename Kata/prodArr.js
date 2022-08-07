16cf9ff0c3e5ae614b6933c1dfd5b495a9bac641
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-07 13:10:48 -0400

    Update prodArr.js

--- Kata/prodArr.js
+++ Kata/prodArr.js
@@ -4,3 +4,12 @@ Calculate the product of all elements in an array.
 
 If the array is null or is empty, the function should return null.
 */
+
+/**
+ * Returns product of all numbers in a numeric array.
+ * Returns null if param is null or array is empty.
+ * @param {Array} values - The array containing the items.
+ */
+function product(values) {
+  return values=== null || values.length===0 ? null: values.reduce( (a,b) => a * b);
+}
