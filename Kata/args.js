5919a24390352016d8d64adefda4702dbdb53faf
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-09 19:30:13 -0400

    Update args.js

--- Kata/args.js
+++ Kata/args.js
@@ -7,5 +7,5 @@ args_count(1, 2, 3, 10) -> 4
 
 // Create a function called args_count that returns the number of arguments provided
 function args_count() {
-  return arguments.length
+  return arguments.length;
 }
