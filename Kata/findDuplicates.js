f9089b04241ffa2d3005a3df2c62b812d8be35ef
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-10 10:16:22 -0400

    Update findDuplicates.js

--- Kata/findDuplicates.js
+++ Kata/findDuplicates.js
@@ -12,7 +12,7 @@ Examples
 
 function duplicates(arr) {
   let newArr = [];
-  let finalArr = []
+  let finalArr = [];
   for(let i = 0; i < arr.length; i++){
     if(!newArr.includes(arr[i])) newArr.push(arr[i])
     else if(finalArr.includes(arr[i])) continue
