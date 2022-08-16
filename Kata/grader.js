d37c47c4f37100f449971aeabaae9619934491cc
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-15 20:24:10 -0400

    Update grader.js

--- Kata/grader.js
+++ Kata/grader.js
@@ -18,7 +18,7 @@ grader(0.6) should be "D"
 */
 
 function grader(score) {
-  if(score > 1 || score < 0.6) return "F"
+  if(score > 1 || score < 0.6) return "F";
   else if(score >= .9) return "A"
   else if(score >= .8) return "B"
   else if(score >= .7) return "C"
