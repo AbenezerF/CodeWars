7d4549081451c420f18376673729e9402ee79ec4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-22 08:59:07 -0400

    Update avgScores.js

--- Kata/avgScores.js
+++ Kata/avgScores.js
@@ -1,1 +1,5 @@
 // Average Scores (7kyu)
+
+/*
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+*/
