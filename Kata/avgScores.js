4cadb3e2f254b23c1057952d83d30eefa0289386
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-22 08:59:27 -0400

    Update avgScores.js

--- Kata/avgScores.js
+++ Kata/avgScores.js
@@ -4,3 +4,8 @@
 Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. 
 You are not allowed to use any loops (including for, for/in, while, and do/while loops).
 */
+
+function average(scores) {
+   let total = scores.reduce( (acc,prev) => acc + prev)
+   return Math.round(total/scores.length)
+}
