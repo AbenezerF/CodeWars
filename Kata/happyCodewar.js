716d7cef66b528e510fcde67f714235eb0ee3598
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-28 09:35:08 -0400

    Update happyCodewar.js

--- Kata/happyCodewar.js
+++ Kata/happyCodewar.js
@@ -13,3 +13,16 @@ var boris  = new Warrior("Boris")
 albert.toString() --> "Hi! my name's Boris" <-- ohoh..
 
 */
+
+function Warrior(n){
+  let name = n;  
+  this.name = function(n){
+    if( n ) name=n;
+    return name;
+  }
+  
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+ this.name();
+}
