b069be8c794cc2c4c8803c829aecb254dd2ce39f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-28 09:34:52 -0400

    Update happyCodewar.js

--- Kata/happyCodewar.js
+++ Kata/happyCodewar.js
@@ -1,1 +1,15 @@
 // Hello Happy Codevarrior! (8Kyu)
+
+/*
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+
+*/
