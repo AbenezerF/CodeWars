5b1f4c9dbe1a13fb9284e900d39e4391534d6c83
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-30 07:54:22 -0400

    Update sumOdd.js

--- Kata/sumOdd.js
+++ Kata/sumOdd.js
@@ -8,5 +8,5 @@ The function should return undefined if any of the values aren't numbers.
 function cubeOdd(arr) {
   if(arr.includes('a') || arr.length === 0) return undefined;
 let newArr = arr.filter( x => x % 2 !== 0).map( x => x ** 3);
-return newArr.reduce( (x,y) => x + y, 0)
+return newArr.reduce( (x,y) => x + y, 0);
 }
