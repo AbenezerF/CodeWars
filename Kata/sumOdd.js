56c04873ea473c4df69e601b04efb90d54e50885
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-30 07:53:03 -0400

    Update sumOdd.js

--- Kata/sumOdd.js
+++ Kata/sumOdd.js
@@ -1,1 +1,5 @@
 // Sum of Odd Cubed Numbers (7Kyu)
+
+/*
+Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+*/
