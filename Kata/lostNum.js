fbf30458a50bdb87d6c0f52e99b3bb8362c07599
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-31 07:00:19 -0400

    Update lostNum.js

--- Kata/lostNum.js
+++ Kata/lostNum.js
@@ -1,5 +1,4 @@
 // Lost number in number sequence (7kyu)
-
 /*
 An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
 
