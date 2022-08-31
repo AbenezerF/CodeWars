0c6aa8b70794afb4d92fe3a7ba318a85e4baaf87
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-08-31 06:57:59 -0400

    Update lostNum.js

--- Kata/lostNum.js
+++ Kata/lostNum.js
@@ -1,1 +1,11 @@
 // Lost number in number sequence (7kyu)
+
+
+
+function findDeletedNumber(arr, mixArr) {
+  let newArr = mixArr.sort( (a,b) => a -b)
+  if(arr.length === newArr.length) return 0
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i] !== newArr[i]) return arr[i]
+  }
+}
