ba474da423e6700238aa15100067b92b4a44708a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-01 08:54:26 -0400

    Update consecItems.js

--- Kata/consecItems.js
+++ Kata/consecItems.js
@@ -10,7 +10,7 @@ It is guaranteed that a and b are both present in arr.
 
 function consecutive(arr, a, b) {
   for(let i = 0; i < arr.length; i++){
-    if(arr[i] === a && (arr[i+1] === b || arr[i-1] === b)) return true
+    if(arr[i] === a && (arr[i+1] === b || arr[i-1] === b)) return true;
   }
   return false;
 }
