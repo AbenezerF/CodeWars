73bf95a1791ca140c5b3dccf5c06aea2c4cebd30
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-01 08:54:11 -0400

    Update consecItems.js

--- Kata/consecItems.js
+++ Kata/consecItems.js
@@ -12,5 +12,5 @@ function consecutive(arr, a, b) {
   for(let i = 0; i < arr.length; i++){
     if(arr[i] === a && (arr[i+1] === b || arr[i-1] === b)) return true;
   }
-  return false
+  return false;
 }
