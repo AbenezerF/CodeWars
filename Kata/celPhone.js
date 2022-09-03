906ccb637e048f3cc6b33fa2165eb7d296a370a5
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-03 09:53:06 -0400

    Update celPhone.js

--- Kata/celPhone.js
+++ Kata/celPhone.js
@@ -16,7 +16,7 @@ function createPhoneNumber(numbers){
   let format = "(xxx) xxx-xxxx";
   
   for(var i = 0; i < numbers.length; i++){
-    format = format.replace('x', numbers[i])
+    format = format.replace('x', numbers[i]);
   }
   return format
 }
