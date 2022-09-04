50ade535d28b592c08bced91df817fa33c3a7855
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-04 13:33:06 -0400

    Update arrFilter.js

--- Kata/arrFilter.js
+++ Kata/arrFilter.js
@@ -10,5 +10,5 @@ getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 
 
 function getEvenNumbers(numbersArray){
-  return numbersArray.filter( (x) => x % 2 === 0);
+  return numbersArray.filter( (x) => x % 2 === 0)
 }
