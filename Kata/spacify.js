ae4b581325361cb440d3792cb0bd87b6c9f574bb
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-06 08:55:19 -0400

    Update spacify.js

--- Kata/spacify.js
+++ Kata/spacify.js
@@ -5,3 +5,7 @@ Modify the spacify function so that it returns the given string with spaces inse
 
 spacify("hello world") // returns "h e l l o   w o r l d"
 */
+
+function spacify(str) {
+  return str.split('').join(' ')
+}
