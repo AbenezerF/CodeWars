ffd058f184c8d7048b2f68cc6f04cf64cc01cb16
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-08 08:08:04 -0400

    Update getFull.js

--- Kata/getFull.js
+++ Kata/getFull.js
@@ -1,1 +1,13 @@
 // FIXME: Get Full Name (7Kyu)
+
+/*
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+Task
+Fix the bug so we can all go home early.
+*/
