3be8fa33863a03573df96108d1d6b661c21dc0be
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-09 07:37:13 -0400

    Update reverse.js

--- Kata/reverse.js
+++ Kata/reverse.js
@@ -7,7 +7,7 @@ Write a function reverse which reverses a list (or in clojure's case, any list-l
 */
 
 reverse = function(array) {
- let arr = []
+ let arr = [];
  for(let i = array.length-1; i >= 0; i--){ arr.push(array[i])}
   return arr;
 }
