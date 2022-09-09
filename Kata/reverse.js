c6a7b498c27c0fef664858b501039a9d0333f8ce
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-09 07:36:31 -0400

    Update reverse.js

--- Kata/reverse.js
+++ Kata/reverse.js
@@ -1,1 +1,7 @@
 // esreveR (7Kyu)
+
+/*
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+*/
