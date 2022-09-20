518b51fdadea4928a37ec67199695eb62677060d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-20 07:29:19 -0400

    Update missingNum.js

--- Kata/missingNum.js
+++ Kata/missingNum.js
@@ -12,7 +12,7 @@ function missingNo(nums) {
   let arr = nums.sort( (a,b) => a - b);
   for(let i =0; i < arr.length; i++){
     if( (arr[i] + 1) !== arr[i+1]){
-      return arr[i] + 1;
+      return arr[i] + 1
     }
   }
 } 
