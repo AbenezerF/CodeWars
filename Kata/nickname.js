2700a96d106c2e67344d9efb16dd83627ecc6ae7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-23 09:10:05 -0400

    Update nickname.js

--- Kata/nickname.js
+++ Kata/nickname.js
@@ -1,1 +1,13 @@
 // Nickname Generator (7Kyu)
+
+
+function nicknameGenerator(name){
+  let arr = ['a','e','i','o','u']
+  if(name.length <= 3){
+    return "Error: Name too short"
+  }else if(arr.includes(name[2])){
+    return name.substring(0,4)
+  }else if(!arr.includes(name[2])){
+    return  name.substring(0,3)
+  }
+}
