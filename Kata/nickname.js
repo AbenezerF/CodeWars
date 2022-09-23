59d7bd7876b3067c1014cdc3ff4770faf74d48da
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-23 09:10:53 -0400

    Update nickname.js

--- Kata/nickname.js
+++ Kata/nickname.js
@@ -29,7 +29,7 @@ The input can be modified
 function nicknameGenerator(name){
   let arr = ['a','e','i','o','u']
   if(name.length <= 3){
-    return "Error: Name too short"
+    return "Error: Name too short";
   }else if(arr.includes(name[2])){
     return name.substring(0,4)
   }else if(!arr.includes(name[2])){
