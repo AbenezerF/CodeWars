3f454c2c5e21874422502ad6115031a9c6086e1a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-09-25 10:21:15 -0400

    Update largestElem.js

--- Kata/largestElem.js
+++ Kata/largestElem.js
@@ -8,3 +8,13 @@ Example:
 largest(2, [7,6,5,4,3,2,1])
 // => [6,7]
 */
+
+
+function largest(n,xs){
+  let final = []
+  let arr = xs.sort( (a,b) => a -b)
+  for(let i = arr.length-n; i < arr.length; i++){
+    final.push(arr[i])
+  }
+  return final
+}
