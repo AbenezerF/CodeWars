444a25cdcb6e255f14a9b960cfed3ae20d505772
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-08 11:28:21 -0400

    Update leoCap.js

--- Kata/leoCap.js
+++ Kata/leoCap.js
@@ -14,7 +14,7 @@ if it was over 88 you should return "Leo got one already!"
 
 function leo(oscar){
   if(oscar > 88) return "Leo got one already!";
-  else if(oscar === 88)  return "Leo finally won the oscar! Leo is happy"
+  else if(oscar === 88)  return "Leo finally won the oscar! Leo is happy";
   else if(oscar === 86)  return "Not even for Wolf of wallstreet?!"
   else return "When will you give Leo an Oscar?"
 }
