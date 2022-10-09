06df7d4f248a308405e34a7468466551a40941ca
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-09 08:29:35 -0400

    Update forLoop.js

--- Kata/forLoop.js
+++ Kata/forLoop.js
@@ -48,7 +48,7 @@ I've defined two array odd and even in the function, and also wrote the return s
 */
 
 function pickIt(arr){
-  var odd=[],even=[]
+  var odd=[],even=[];
   for(let i = 0; i < arr.length; i++){
     if(arr[i] % 2 === 0) even.push(arr[i])
     else odd.push(arr[i])
