3af11312d1b44db263c52b4cfcf5e836d3db604d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-11 08:42:46 -0400

    Update whoAteCookie.js

--- Kata/whoAteCookie.js
+++ Kata/whoAteCookie.js
@@ -11,7 +11,7 @@ Please leave feedback for this kata. Cheers!
 */
 
 function cookie(x){
-  if(typeof x === 'string') return "Who ate the last cookie? It was Zach!"
+  if(typeof x === 'string') return "Who ate the last cookie? It was Zach!";
   else if(typeof x === 'number') return "Who ate the last cookie? It was Monica!"
   else return "Who ate the last cookie? It was the dog!"
 }
