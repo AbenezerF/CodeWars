98993c8eec4d1ada98e2b271766b19a9ebdff83a
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-14 09:03:54 -0400

    Update classyClasses.js

--- Kata/classyClasses.js
+++ Kata/classyClasses.js
@@ -8,3 +8,14 @@ Your task is to complete this Class, the Person class has been created.
 You must fill in the Constructor method to accept a name as string and an age as number, 
 complete the get Info property and getInfo method/Info getter which should return johns age is 34
 */
+
+
+class Person {
+  constructor(string, age) {
+    this.string = string
+    this.age = age
+  }
+  get info(){
+    return `${this.string}s age is ${this.age}`
+  }
+}
