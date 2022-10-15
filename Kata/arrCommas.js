5d3ae275b4c40c20f78d0d9a5fb60bdc78f61ad4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-15 11:25:16 -0400

    Update arrCommas.js

--- Kata/arrCommas.js
+++ Kata/arrCommas.js
@@ -1,1 +1,10 @@
 // Printing Array elements with Comma delimiters(8Kyu)
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
