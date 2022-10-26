19bf4f465c12e48c064497434b78c6bc5efe0180
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-10-26 09:55:43 -0400

    Update betweenNumbers.js

--- Kata/betweenNumbers.js
+++ Kata/betweenNumbers.js
@@ -1,1 +1,17 @@
 //Get the integers between two numbers (7kyu)
+
+/*
+Build a function that can get all the integers between two given numbers.
+
+Example:
+
+(2,9)
+
+Should give you this output back:
+
+[ 3, 4, 5, 6, 7, 8 ]
+
+If startNum is the same as endNum, return an empty array.
+
+
+*/
