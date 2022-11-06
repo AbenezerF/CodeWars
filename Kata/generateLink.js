7866a29f1f1b999c34ff2077172c333548d1c685
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-11-06 10:00:45 -0500

    Update generateLink.js

--- Kata/generateLink.js
+++ Kata/generateLink.js
@@ -11,5 +11,5 @@ use this as a reference encoding
 */
 
 function generateLink(user) {
-  return `http://www.codewars.com/users/${encodeURIComponent(user)}`
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
 }
