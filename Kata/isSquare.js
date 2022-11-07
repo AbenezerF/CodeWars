e2b4620309e0080a20552e7f66d27163e611ef75
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-11-07 09:24:42 -0500

    Update isSquare.js

--- Kata/isSquare.js
+++ Kata/isSquare.js
@@ -23,7 +23,7 @@ is_square([]) --> None
 var isSquare = function(arr){
   if(arr.length === 0) return undefined;
   for(let i = 0; i < arr.length; i++){
-    if( (Math.sqrt(arr[i]) * Math.sqrt(arr[i])) !== arr[i]) return false
+    if( (Math.sqrt(arr[i]) * Math.sqrt(arr[i])) !== arr[i]) return false;
   }
    return true
 }
