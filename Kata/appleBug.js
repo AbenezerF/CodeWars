c3cf9d96c9f8a7d9ff2b028cea067559fe6f44f2
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-11-10 09:06:16 -0500

    Update appleBug.js

--- Kata/appleBug.js
+++ Kata/appleBug.js
@@ -11,3 +11,11 @@ input: string Array apple
 
 output: Location of "B", [x,y]
 */
+
+function sc(apple){
+  for(let i = 0; i < apple.length; i++){
+    for(let j = 0; j < apple[i].length; j++){
+      if(apple[i][j] == "B") return [i,j]
+    }
+  }
+}
