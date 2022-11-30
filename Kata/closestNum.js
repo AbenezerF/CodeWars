d554594178f954b529105fa10b4cd1c199b740ea
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-11-30 07:52:39 -0500

    Update closestNum.js

--- Kata/closestNum.js
+++ Kata/closestNum.js
@@ -1,1 +1,15 @@
-// Return the closest number multiple of 10(7Kyu)
+// Return the closest number multiple of 10 (7Kyu)
+/*
+Given a number return the closest number to it that is divisible by 10.
+
+Example input:
+
+22
+25
+37
+Expected output:
+
+20
+30
+40
+*/
