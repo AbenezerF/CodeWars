4182c9b16e9c4ee2d8b47d2ee02fdbf3755650eb
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-02 10:03:02 -0500

    Update reverseStrings.js

--- Kata/reverseStrings.js
+++ Kata/reverseStrings.js
@@ -13,7 +13,7 @@ function spinWords(string){
   let str = string.split(' ');
   for(let i = 0; i < str.length; i++){
     if(str[i].length >= 5){
-      str[i] = [...str[i]].reverse().join('')
+      str[i] = [...str[i]].reverse().join('');
     }
   }
   return str.join(' ')
