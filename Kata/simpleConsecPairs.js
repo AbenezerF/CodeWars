69d2a5e3076608fd21ea91f2abad198a96783af4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-03 07:11:42 -0500

    Update simpleConsecPairs.js

--- Kata/simpleConsecPairs.js
+++ Kata/simpleConsecPairs.js
@@ -17,7 +17,7 @@ Good luck!
 function pairs(ar){
  let count = 0;
    for(let i = 0; i < ar.length-1; i+=2){
-     if(ar[i] + 1 === ar[i+1] || ar[i] - 1 === ar[i+1]) count++
+     if(ar[i] + 1 === ar[i+1] || ar[i] - 1 === ar[i+1]) count++;
    }
   return count
 };
