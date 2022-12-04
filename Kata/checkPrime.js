60d91998ce68f6f548a722a6c456c3097eeb56a0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-04 11:09:32 -0500

    Update checkPrime.js

--- Kata/checkPrime.js
+++ Kata/checkPrime.js
@@ -17,6 +17,6 @@ Examples(input --> output)
 function isPrime(n) {
     for (let i = 2; i * i <= n; i++)
         if (n % i === 0)
-          return false
+          return false;
     return n > 1;
 }
