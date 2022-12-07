838ca180563776ebcbbc877a59ab8d90bcfc1e5f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-07 08:10:37 -0500

    Update uniqueOrder.js

--- Kata/uniqueOrder.js
+++ Kata/uniqueOrder.js
@@ -14,8 +14,8 @@ var uniqueInOrder=function(iterable){
    let arr = [];
   for(let i = 0; i < iterable.length; i++){
     if(iterable[i] !== iterable[i+1]){
-      arr.push(iterable[i]);
+      arr.push(iterable[i])
     }
   }
-  return typeof arr === 'string' ? arr.map( (x) => x.toUpperCase()) : arr;
+  return typeof arr === 'string' ? arr.map( (x) => x.toUpperCase()) : arr
 }
