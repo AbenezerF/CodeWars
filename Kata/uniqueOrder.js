ff5db42aa389bafa234001221633115d2d53ab95
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-07 08:09:58 -0500

    Update uniqueOrder.js

--- Kata/uniqueOrder.js
+++ Kata/uniqueOrder.js
@@ -9,3 +9,13 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
+
+var uniqueInOrder=function(iterable){
+   let arr = []
+  for(let i = 0; i < iterable.length; i++){
+    if(iterable[i] !== iterable[i+1]){
+      arr.push(iterable[i])
+    }
+  }
+  return typeof arr === 'string' ? arr.map( (x) => x.toUpperCase()) : arr
+}
