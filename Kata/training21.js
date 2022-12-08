49784328f66c6661d6801442006db2dc710310b5
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-08 09:50:07 -0500

    Update training21.js

--- Kata/training21.js
+++ Kata/training21.js
@@ -93,7 +93,7 @@ xyxyxyxyxy  <---The effect when you console.log it
 function fiveLine(s){
   //coding here...
   s = s.trim();
-  var tmp = s
+  var tmp = s;
   var res = [s]
   
   for (var i = 1; i < 5; i++) {
