98d1c58f228c6aacbf932ae8601dc6cc851bedd5
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-08 09:49:16 -0500

    Update training21.js

--- Kata/training21.js
+++ Kata/training21.js
@@ -1,1 +1,14 @@
 // Training JS #21: Methods of String object--trim() and the string template(8kyu)
+
+
+function fiveLine(s){
+  //coding here...
+  s = s.trim();
+  var tmp = s;
+  var res = [s];
+  
+  for (var i = 1; i < 5; i++) {
+   	res.push(s += tmp);
+  } 
+  return res.join("\n");
+}
