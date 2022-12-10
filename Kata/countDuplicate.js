5ed7cb2dec4caee074e0e4cba9a10c1a07ff4d2d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-10 11:21:59 -0500

    Update countDuplicate.js

--- Kata/countDuplicate.js
+++ Kata/countDuplicate.js
@@ -13,3 +13,18 @@ Example
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice
 */
+
+function duplicateCount(text){
+  var lower = text.toLowerCase();
+  var count = 0;
+  var used = [];
+  
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+  
+  return count;
+}
