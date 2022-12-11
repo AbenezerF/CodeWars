5b35f2934c7a229d30ccaaafc6ac7526c2c4aa9c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-11 10:24:53 -0500

    Update numInBus.js

--- Kata/numInBus.js
+++ Kata/numInBus.js
@@ -18,8 +18,8 @@ The second value in the first integer array is 0, since the bus is empty in the 
 var number = function(busStops){
 	let totalPeople = 0;
   for (var i=0; i<busStops.length; i++) {
-  	totalPeople += busStops[i][0];
-    totalPeople -= busStops[i][1];
+  	totalPeople += busStops[i][0]
+    totalPeople -= busStops[i][1]
   }
-  return totalPeople;
+  return totalPeople
 }
