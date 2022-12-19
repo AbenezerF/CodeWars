414c7dee74bf4551b045582bca42c4887563abcb
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-19 10:08:43 -0500

    Update sorted.js

--- Kata/sorted.js
+++ Kata/sorted.js
@@ -17,5 +17,5 @@ let des = true;
     if(array[i] > array[i-1]) des = false;
     if(array[i] < array[i-1]) asc = false;
   }
-  return asc ? 'yes, ascending' : des ? 'yes, descending' : 'no';
+  return asc ? 'yes, ascending' : des ? 'yes, descending' : 'no'
 }
