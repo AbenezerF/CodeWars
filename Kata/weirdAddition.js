d44903437099224f8fb55d3f982f5e8525cec564
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-27 10:45:58 -0500

    Update weirdAddition.js

--- Kata/weirdAddition.js
+++ Kata/weirdAddition.js
@@ -1,1 +1,12 @@
 // 16+18=214 (7Kyu)
+/*
+For this kata you will have to forget how to add two numbers.
+
+It can be best explained using the following meme:
+
+In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
+
+You may assume both integers are positive integers.
+
+*/
+
