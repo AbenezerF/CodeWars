0c1d67997b5d4712bcea3e5a9af62e06076e604b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2022-12-31 11:47:09 -0500

    Update tipCalc.js

--- Kata/tipCalc.js
+++ Kata/tipCalc.js
@@ -24,6 +24,6 @@ function calculateTip(amount, rating) {
   else if(rating.toLowerCase() === 'poor') return Math.ceil(amount * 0.05);
   else if(rating.toLowerCase() === 'good') return Math.ceil(amount * 0.1);
   else if(rating.toLowerCase() === 'great') return Math.ceil(amount * 0.15);
-  else if(rating.toLowerCase() === 'excellent') return Math.ceil(amount * 0.2)
-  else return "Rating not recognised"
+  else if(rating.toLowerCase() === 'excellent') return Math.ceil(amount * 0.2);
+  else return "Rating not recognised";
 }
