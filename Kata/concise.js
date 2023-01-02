22c0698956377b874369d87b5d8e37032afca9c9
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-02 11:22:37 -0500

    Update concise.js

--- Kata/concise.js
+++ Kata/concise.js
@@ -18,5 +18,5 @@ Whatever you do, do not change what the function does. Good luck :)
 */
 
 function describeAge(age) {
-  return "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly")
+  return "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly");
 }
