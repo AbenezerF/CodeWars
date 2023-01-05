a6ef53e8f93be6678db0b8ecf4f0393f2b01b6c3
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-05 11:16:44 -0500

    Update freqItem.js

--- Kata/freqItem.js
+++ Kata/freqItem.js
@@ -7,3 +7,11 @@ input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
 ouptut: 5 
 The most frequent number in the array is -1 and it occurs 5 times.
 */
+
+function mostFrequentItemCount(collection) {
+  let obj = {}
+  collection.forEach(x => {
+    obj[x] = (obj[x] || 0) + 1
+  })
+  return collection.length === 0 ? 0 : Math.max(...Object.values(obj))
+}
