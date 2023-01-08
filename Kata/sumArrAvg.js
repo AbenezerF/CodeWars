fc6668a5adadeadb3e189ae5bccd0e26e47d9265
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-08 11:13:19 -0500

    Update sumArrAvg.js

--- Kata/sumArrAvg.js
+++ Kata/sumArrAvg.js
@@ -22,6 +22,6 @@ math.floor(44.2) = 44
 */
 
 const sumAverage = arr => {
-  const result = arr.map(x => x.reduce((a, b) => a + b) / x.length).reduce((a, b) => a + b)
+  const result = arr.map(x => x.reduce((a, b) => a + b) / x.length).reduce((a, b) => a + b);
   return Math.floor(result)
 }
