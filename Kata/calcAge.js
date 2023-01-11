bd801a827d32e192bda143a8ca0543ebd8cabf21
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-11 08:09:33 -0500

    Update calcAge.js

--- Kata/calcAge.js
+++ Kata/calcAge.js
@@ -7,3 +7,12 @@ difference < 0
 Either of the calculated ages come out to be negative
 
 */
+
+function getAges(sum,difference){
+
+  let average = 0;
+  if (sum < 0 || difference < 0 || sum - difference < 0) return null;
+  else average = (sum - difference) /2;
+  
+  return [average+difference, average];
+};
