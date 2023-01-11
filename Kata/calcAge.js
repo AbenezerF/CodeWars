6720c2e933d42ee886e9bf5cc104918ef793cf99
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-11 08:09:50 -0500

    Update calcAge.js

--- Kata/calcAge.js
+++ Kata/calcAge.js
@@ -12,7 +12,7 @@ function getAges(sum,difference){
 
   let average = 0;
   if (sum < 0 || difference < 0 || sum - difference < 0) return null;
-  else average = (sum - difference) /2
+  else average = (sum - difference) /2;
   
   return [average+difference, average]
 };
