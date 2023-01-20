d184e116915c33400fc51667aec106dcfca43cc0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-20 10:16:59 -0500

    Update factorialFctory.js

--- Kata/factorialFctory.js
+++ Kata/factorialFctory.js
@@ -15,7 +15,7 @@ For more on Factorials : http://en.wikipedia.org/wiki/Factorial
 function factorial (n) {
   let total = 1
   for(let i = n; i > 0; i--){
-    total *= i
+    total *= i;
   }
   return n < 0 ? null : total
 }
