6581f3e4dc5f3719afb6a247bff0721f147bfa4c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-24 08:35:41 -0500

    Update leetSpeak.js

--- Kata/leetSpeak.js
+++ Kata/leetSpeak.js
@@ -10,3 +10,40 @@ For example:
 
 toLeetSpeak("LEET") returns "1337"
 */
+
+function toLeetSpeak(str) {
+  let obj = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2'
+}
+  let final = ''
+  for(let i = 0; i < str.length; i++){
+    if(obj[str[i]]) final += (obj[str[i]])
+    else if( str[i] = ' ') final += str[i]
+  }
+  return final;
+}
