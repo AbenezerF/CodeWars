8a2d5f6911efa9fe983a48ad0b2aea0a96b50c3c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-01-24 08:35:21 -0500

    Update leetSpeak.js

--- Kata/leetSpeak.js
+++ Kata/leetSpeak.js
@@ -1,1 +1,12 @@
 // ToLeetSpeak (7Kyu)
+/*
+Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+For example:
+
+toLeetSpeak("LEET") returns "1337"
+*/
