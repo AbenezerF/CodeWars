48e033c86fa289580da6b60d0c6be31e5d2b997f
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-02-03 12:25:18 -0500

    Update vaporcode.js

--- Kata/vaporcode.js
+++ Kata/vaporcode.js
@@ -9,3 +9,7 @@ Examples
 "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 */
+
+function vaporcode(string) {
+  return string.toUpperCase().split(' ').join('').split('').join('  ')
+}
