94d8c28ac21c159478624de585e8bd4a2e84e33b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-02-06 10:53:51 -0500

    Update five.js

--- Kata/five.js
+++ Kata/five.js
@@ -22,7 +22,7 @@ I have also created other katas. Take a look if you enjoyed this kata!
 function dontGiveMeFive(start, end){
   let arr = [];
   for (let i = start; i <= end; i++) {
-    if (!i.toString().includes('5')) arr.push(i)
+    if (!i.toString().includes('5')) arr.push(i);
   }
   return arr.length
 }
