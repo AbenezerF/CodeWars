162d3dae609636bcb2315b9ac003307fefc4283b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-02-14 10:06:24 -0500

    Update bugFixing.js

--- Kata/bugFixing.js
+++ Kata/bugFixing.js
@@ -6,8 +6,8 @@ Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluat
 
 function evalObject(value){
   switch(value.operation){
-    case'+': return value.a + value.b
-    case'-': return value.a - value.b
+    case'+': return value.a + value.b;
+    case'-': return value.a - value.b;
     case'/': return value.a / value.b
     case'*': return value.a * value.b
     case'%': return value.a % value.b
