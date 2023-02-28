e36d0e854b56481ce587b56915269c720402d008
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-02-28 08:02:06 -0500

    Update listFiltering.java

--- javaKata/listFiltering.java
+++ javaKata/listFiltering.java
@@ -7,3 +7,17 @@ Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
 Kata.filterList(List.of(1, 2, "a", "b", 0, 15)) => List.of(1,2,0,15)
 Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)) => List.of(1, 2, 231)
 */
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Kata {
+  
+  public static List filterList(final List<Object> list) {
+    return list.stream()
+      .filter(e -> e instanceof Integer)
+      .collect(Collectors.toList());
+  }
+
+}
