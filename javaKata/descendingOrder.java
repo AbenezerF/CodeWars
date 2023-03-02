7658b1b11f202f8b11927862737a6c4fee71e2f4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-02 10:27:06 -0500

    Update descendingOrder.java

--- javaKata/descendingOrder.java
+++ javaKata/descendingOrder.java
@@ -11,3 +11,16 @@ Input: 123456789 Output: 987654321
 
 
 */
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescendingOrder {
+  public static int sortDesc(final int num) {
+    String[] array = String.valueOf(num).split("");
+    Arrays.sort(array, Collections.reverseOrder());
+    return Integer.valueOf(String.join("", array));
+  }
+}
+ 
