c108dbb4e45c0ba8eb270cb9da81ede187c20a28
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-03 09:48:39 -0500

    Update evenOrOdd.java

--- javaKata/evenOrOdd.java
+++ javaKata/evenOrOdd.java
@@ -36,3 +36,14 @@ public class Codewars {
   }
 }
   
+
+// Solution using ternary operator
+public class Codewars {
+  public static String oddOrEven (int[] array) {
+    int sum = 0;
+    for (int n : array){
+      sum += n;
+    }
+    return sum%2==0 ? "even" : "odd";
+  }
+}
