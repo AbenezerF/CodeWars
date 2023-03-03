32c4b2f8ed50437f0bc445446cef700fa11df747
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-03 09:48:02 -0500

    Update evenOrOdd.java

--- javaKata/evenOrOdd.java
+++ javaKata/evenOrOdd.java
@@ -21,3 +21,18 @@ Have fun!
 
 
 */
+
+
+public class Codewars {
+  public static String oddOrEven (int[] array) {
+  int total = 0;
+  for(int i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  if(total % 2 == 0){
+    return "even";
+  }
+  return "odd";
+  }
+}
+  
