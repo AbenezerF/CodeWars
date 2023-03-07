330b4e8aeb96c3d863742be17efb18e6784c80de
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-07 09:38:14 -0500

    Update strRepeat.java

--- javaKata/strRepeat.java
+++ javaKata/strRepeat.java
@@ -7,3 +7,14 @@ Examples (input -> output)
 6, "I"     -> "IIIIII"
 5, "Hello" -> "HelloHelloHelloHelloHello"
 */
+
+
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      String total = "";
+      for(int i = 0; i < repeat; i++){
+        total += string;
+      }
+        return total;
+    }
+}
