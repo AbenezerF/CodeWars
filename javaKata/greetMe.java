cdb447d0318efaaa3d30804f9f4fe092e66556b7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-08 09:09:34 -0500

    Update greetMe.java

--- javaKata/greetMe.java
+++ javaKata/greetMe.java
@@ -11,7 +11,6 @@ Example:
 
 public class GreetMe{
   public static String greet(String name){
-    String firstChar = name.substring(0,1);
-    return "Hello " + firstChar.toUpperCase() + name.toLowerCase().substring(1) + "!";
+    return "Hello " + name.substring(0,1).toUpperCase() + name.toLowerCase().substring(1) + "!";
   }
 }
