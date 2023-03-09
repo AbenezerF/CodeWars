d915f4ecf3fd1c6ec613797b6ce247a36f4f626d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-09 07:24:40 -0500

    Update unexpectedParsing.java

--- javaKata/unexpectedParsing.java
+++ javaKata/unexpectedParsing.java
@@ -22,3 +22,20 @@ Function should return a dictionary/Object/Hash with "status" as a key, whose va
 
 But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
 */
+
+import java.util.HashMap;
+
+public class Kata {
+
+    public static HashMap <String, String> getStatus(boolean isBusy) {
+        HashMap<String, String> status = new HashMap<String, String>();
+      
+        if (isBusy) {
+          status.put("status", "busy");
+        } else {
+          status.put("status", "available");
+        }
+        return status;
+    }
+  
+}
