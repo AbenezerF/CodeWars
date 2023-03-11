ab29d793209f7147c9ec7d9e05a139de7351ed50
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-11 08:57:46 -0500

    Update determineSex.java

--- javaKata/determineSex.java
+++ javaKata/determineSex.java
@@ -9,3 +9,9 @@ Determine if the sex of the offspring will be male or female based on the X or Y
 
 If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 */
+
+public class Kata {
+  public static String chromosomeCheck(String sperm) {
+    return sperm == "XX" ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son.";
+  }
+}
