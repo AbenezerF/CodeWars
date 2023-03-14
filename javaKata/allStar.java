9f053c0babd576d324d4501d60b20f98b69e75fd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-14 10:17:28 -0400

    Update allStar.java

--- javaKata/allStar.java
+++ javaKata/allStar.java
@@ -1,4 +1,4 @@
-// All Star Code Challenge #18
+// All Star Code Challenge #18 (8Kyu)
 
 /*
 Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
