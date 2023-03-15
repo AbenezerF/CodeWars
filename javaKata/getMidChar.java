05f262412b198b20bd7b9a6b6e94af56a1a5924e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-15 10:54:57 -0400

    Update getMidChar.java

--- javaKata/getMidChar.java
+++ javaKata/getMidChar.java
@@ -1,1 +1,10 @@
 // Get the Middle Character (7Kyu)
+
+
+
+class Kata {
+  public static String getMiddle(String word) {
+    int middleChar = (int)(word.length() - 1) / 2;
+    return word.length() % 2 == 0 ? word.substring(middleChar, middleChar + 2) : word.substring(middleChar, middleChar + 1);
+  }
+}
