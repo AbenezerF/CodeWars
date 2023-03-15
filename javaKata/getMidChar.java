29794ec58f4bf57232616f91834a86467a5c431b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-15 10:55:35 -0400

    Update getMidChar.java

--- javaKata/getMidChar.java
+++ javaKata/getMidChar.java
@@ -25,6 +25,6 @@ The middle character(s) of the word represented as a string.
 class Kata {
   public static String getMiddle(String word) {
     int middleChar = (int)(word.length() - 1) / 2;
-    return word.length() % 2 == 0 ? word.substring(middleChar, middleChar + 2) : word.substring(middleChar, middleChar + 1)
+    return word.length() % 2 == 0 ? word.substring(middleChar, middleChar + 2) : word.substring(middleChar, middleChar + 1);
   }
 }
