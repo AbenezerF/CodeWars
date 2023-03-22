73c4d704dae9bb62b81b41f77d699088387180bd
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-22 07:02:11 -0400

    Update smallestNum.java

--- javaKata/smallestNum.java
+++ javaKata/smallestNum.java
@@ -15,6 +15,6 @@ import java.util.Arrays;
 public class SmallestIntegerFinder {
     public static int findSmallestInt(int[] args) {
       Arrays.sort(args);
-      return args[0]
+      return args[0];
     }
 }
