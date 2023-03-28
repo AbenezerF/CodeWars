25a9da31cc561c529623b51a9007c749a5216665
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-28 09:03:38 -0400

    Update countSheep.java

--- javaKata/countSheep.java
+++ javaKata/countSheep.java
@@ -6,3 +6,13 @@ If you can't sleep, just count sheep!!
 Task:
 Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 */
+
+class Kata {
+  public static String countingSheep(int num) {
+    String str = "";
+    for(int i = 1; i <= num; i++){
+      str += String.valueOf(i) + " sheep...";
+    }
+    return num == 0 ? "" : str;
+  }
+}
