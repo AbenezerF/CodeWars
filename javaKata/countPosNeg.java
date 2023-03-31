408889d8cc2c67fb8c0b1417c5c8a9e6998d521d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-31 11:49:22 -0400

    Update countPosNeg.java

--- javaKata/countPosNeg.java
+++ javaKata/countPosNeg.java
@@ -14,8 +14,7 @@ For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should r
 
 public class Kata{
     public static int[] countPositivesSumNegatives(int[] input){
-      int pos = 0;
-      int neg = 0;
+      int pos = 0, neg = 0;
       if(input == null || input.length == 0) return new int[] {};
       for(int i = 0; i < input.length; i++){
         if(input[i] < 0) neg += input[i];
