7319f5a3837b8318d7315a55dd3450b9905d2a8b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-03-31 11:48:46 -0400

    Update countPosNeg.java

--- javaKata/countPosNeg.java
+++ javaKata/countPosNeg.java
@@ -16,8 +16,7 @@ public class Kata{
     public static int[] countPositivesSumNegatives(int[] input){
       int pos = 0;
       int neg = 0;
-      int[] empty = {};
-      if(input == null || input.length == 0) return empty;
+      if(input == null || input.length == 0) return new int[] {};
       for(int i = 0; i < input.length; i++){
         if(input[i] < 0) neg += input[i];
         if(input[i] > 0) pos++;
