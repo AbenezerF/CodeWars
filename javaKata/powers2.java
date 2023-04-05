bc5dc479cf008878e1167bfb22567c7dd466fda2
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-05 11:59:45 -0400

    Update powers2.java

--- javaKata/powers2.java
+++ javaKata/powers2.java
@@ -9,3 +9,14 @@ n = 1  ==> [1, 2]     # [2^0, 2^1]
 n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
 */
+
+
+public class Kata{
+  public static long[] powersOfTwo(int n){
+    long[] val = new long[n+1];
+    for(int i = 0; i <= n; i++){
+      val[i] = (long)Math.pow(2,i);
+    }
+    return val;
+  }
+}
