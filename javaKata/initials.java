49be875da3465c526eef0802fb2eae70e35c93e6
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-06 07:36:36 -0400

    Update initials.java

--- javaKata/initials.java
+++ javaKata/initials.java
@@ -13,3 +13,14 @@ patrick feeney => P.F
 
 
 */
+
+public class AbbreviateTwoWords {
+
+  public static String abbrevName(String name) {
+    String[] arr = name.split(" ");
+    String first = arr[0].substring(0,1).toUpperCase();
+    String second = arr[1].substring(0,1).toUpperCase();
+
+    return first + "." + second;
+  }
+}
