98a99dbac690f815f713f805c8562d6e0c6ae82b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-09 10:21:27 -0400

    Update returningStr.java

--- javaKata/returningStr.java
+++ javaKata/returningStr.java
@@ -8,6 +8,6 @@ Make a function that will return a greeting statement that uses an input; your p
 
 public class Kata{
   public static String greet(String name){
-    return "Hello, " + name + " how are you doing today?"
+    return "Hello, " + name + " how are you doing today?";
   }
 }
