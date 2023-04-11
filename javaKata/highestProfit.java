b4fe0795ead914dd370303361785c444282f7a09
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-11 07:01:01 -0400

    Update highestProfit.java

--- javaKata/highestProfit.java
+++ javaKata/highestProfit.java
@@ -36,6 +36,6 @@ class MinMax {
     public static int[] minMax(int[] arr) {
         // Your awesome code here
          Arrays.sort(arr);
-        return new int[]{arr[0],arr[arr.length-1]};
+        return new int[]{arr[0],arr[arr.length-1]}
     }
 }
