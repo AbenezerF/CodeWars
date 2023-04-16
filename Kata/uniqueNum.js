4f6ff0cfbc2ce5c82e573e3b9648c44559ad68c1
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-16 06:43:30 -0400

    Update uniqueNum.js

--- Kata/uniqueNum.js
+++ Kata/uniqueNum.js
@@ -16,7 +16,7 @@ function findUniq(arr) {
   for(let i = 0; i < arr.length-1; i++){
     if(i == 0 && arr[i] !== arr[i+1]) return arr[i];
     else{
-      if(arr[arr.length-1] !== arr[arr.length-2]) return arr[arr.length-1] 
+      if(arr[arr.length-1] !== arr[arr.length-2]) return arr[arr.length-1];
     }
   }
 }
