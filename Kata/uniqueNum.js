e2174ed74c9c2e8427b82b876e64edbc789ffd24
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-16 06:43:19 -0400

    Update uniqueNum.js

--- Kata/uniqueNum.js
+++ Kata/uniqueNum.js
@@ -14,7 +14,7 @@ The tests contain some very huge arrays, so think about performance.
 function findUniq(arr) {
   arr = arr.sort( (a,b) => a - b);
   for(let i = 0; i < arr.length-1; i++){
-    if(i == 0 && arr[i] !== arr[i+1]) return arr[i]
+    if(i == 0 && arr[i] !== arr[i+1]) return arr[i];
     else{
       if(arr[arr.length-1] !== arr[arr.length-2]) return arr[arr.length-1] 
     }
