05030c7861c3f73429ebfb3654985994c52abc76
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-16 07:23:29 -0400

    Update sortOdd.js

--- Kata/sortOdd.js
+++ Kata/sortOdd.js
@@ -9,3 +9,13 @@ Examples
 [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 */
+
+function sortArray(array) {
+  let sorted = array.filter( (x) => x % 2 !== 0).sort((a,b) => a - b)
+  for(let i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      sorted.splice(i,0,array[i])
+    }
+  }
+  return sorted
+}
