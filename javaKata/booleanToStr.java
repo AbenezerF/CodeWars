7219aeac51af9162439a4df64dcd31860593487e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-17 08:26:54 -0400

    Update booleanToStr.java

--- javaKata/booleanToStr.java
+++ javaKata/booleanToStr.java
@@ -8,6 +8,6 @@ Note: Only valid inputs will be given.
 
 public class BooleanToString {
   public static String convert(boolean b) {
-    return b ? "true" : "false"
+    return b ? "true" : "false";
   }
 }
