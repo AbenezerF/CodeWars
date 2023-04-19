6e52510bcc3c457adf9526b4f1d80710eeaa8fe7
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-19 07:59:04 -0400

    Update zeroToEnd.js

--- Kata/zeroToEnd.js
+++ Kata/zeroToEnd.js
@@ -5,3 +5,9 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
+
+function moveZeros(arr) {
+  let filtered = arr.filter( (x) => x === 0)
+  arr = arr.filter( (x) => x !== 0)
+  return arr.concat(filtered)
+}
