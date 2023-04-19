36a182c04915457028d88bb91742c1cef30c3b24
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-19 07:43:00 -0400

    Update pigLatin.js

--- Kata/pigLatin.js
+++ Kata/pigLatin.js
@@ -9,3 +9,12 @@ pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world !');     // elloHay orldway !
 
 */
+
+function pigIt(str){
+  let arr = str.split(' ')
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == '!' || arr[i] == '.' || arr[i] == '?') continue
+    arr[i] = arr[i].substring(1) + arr[i].substring(0,1) + 'ay'
+  }
+  return arr.join(' ') 
+}
