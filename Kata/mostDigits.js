bdea972305b93211f80b788bf9dd57033d0b5b67
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-20 17:13:17 -0400

    Update mostDigits.js

--- Kata/mostDigits.js
+++ Kata/mostDigits.js
@@ -11,6 +11,6 @@ function findLongest(array){
   let arr = array.map( (x) => x.toString()).sort( (a,b) => a.length-b.length);
   let maxLength = arr[arr.length-1].length;
   for(let i = 0; i < arr.length; i++){
-    if(arr[i].length === maxLength) return Number(arr[i])
+    if(arr[i].length === maxLength) return Number(arr[i]);
   }
 }
