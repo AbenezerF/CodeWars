2d39006612667c31090e7425ae4ede2bf5f6b202
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-20 17:12:47 -0400

    Update mostDigits.js

--- Kata/mostDigits.js
+++ Kata/mostDigits.js
@@ -7,3 +7,11 @@ If two numbers in the argument array have the same number of digits, return the 
 
 
 */
+
+function findLongest(array){
+  let arr = array.map( (x) => x.toString()).sort( (a,b) => a.length-b.length);
+  let maxLength = arr[arr.length-1].length
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].length === maxLength) return Number(arr[i])
+  }
+}
