e890dfdac8db8954d1f3c50f7c2eed1b58a23019
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-04-28 10:19:47 -0400

    Update isEven.java

--- javaKata/isEven.java
+++ javaKata/isEven.java
@@ -13,3 +13,9 @@ Floats with decimal part non equal to zero are considered UNeven for this kata.
 
 
 */
+
+public class Number {
+  public boolean isEven(double n) {
+    return n % 2 == 0;
+  }
+}
