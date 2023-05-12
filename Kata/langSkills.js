8bc46cc00bdd8c025e8788fff1aeea065674be04
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-05-12 06:30:48 -0400

    Update langSkills.js

--- Kata/langSkills.js
+++ Kata/langSkills.js
@@ -12,3 +12,7 @@ Examples
 {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
 
 */
+
+function myLanguages(results) {
+  return Object.keys(results).filter(r => results[r] > 59).sort((a,b) => results[b] - results[a]);
+}
