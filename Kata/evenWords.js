f4557fd5447c0ad2982e3313bd0aed584f8c8114
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-05-20 11:51:28 -0400

    Update evenWords.js

--- Kata/evenWords.js
+++ Kata/evenWords.js
@@ -9,3 +9,7 @@ var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
 
 console.log(output); // --> ['word', 'word']
 */
+
+function filterEvenLengthWords(words) {
+  return words.filter( x => x.length % 2 === 0)
+}
