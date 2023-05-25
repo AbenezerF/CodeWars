03b753331dc758d4dd60672fd20656e0a509b44d
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-05-25 07:16:14 -0400

    Update reduceGrow.java

--- javaKata/reduceGrow.java
+++ javaKata/reduceGrow.java
@@ -5,3 +5,14 @@ Given a non-empty array of integers, return the result of multiplying the values
 
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 */
+
+public class Kata{
+  public static int grow(int[] x){
+    int total = 1;
+    for(int i = 0; i < x.length; i++){
+      total *= x[i];
+    }
+    return total;
+  }
+
+}
