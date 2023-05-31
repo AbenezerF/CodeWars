d9d6faa735bcda259b2b58d64c78a953c73bf756
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-05-31 07:28:45 -0400

    Update strCamelCase.js

--- Kata/strCamelCase.js
+++ Kata/strCamelCase.js
@@ -10,3 +10,7 @@ Examples
 
 "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
 */
+
+function toCamelCase(str){
+  return str.split(/-|_/g).map((w, i) => (i > 0 ? w.charAt(0).toUpperCase() : w.charAt(0)) + w.slice(1)).join('');
+}
