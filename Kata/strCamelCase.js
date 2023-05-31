4db032750acbd94f10ff83ae4b3c289fdcdc6dd4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-05-31 07:28:53 -0400

    Update strCamelCase.js

--- Kata/strCamelCase.js
+++ Kata/strCamelCase.js
@@ -12,5 +12,5 @@ Examples
 */
 
 function toCamelCase(str){
-  return str.split(/-|_/g).map((w, i) => (i > 0 ? w.charAt(0).toUpperCase() : w.charAt(0)) + w.slice(1)).join('');
+  return str.split(/-|_/g).map((w, i) => (i > 0 ? w.charAt(0).toUpperCase() : w.charAt(0)) + w.slice(1)).join('')
 }
