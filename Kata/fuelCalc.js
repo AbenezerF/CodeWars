a84980c0de8df275236c705319aea23b6e488a90
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-06-19 10:02:06 -0400

    Update fuelCalc.js

--- Kata/fuelCalc.js
+++ Kata/fuelCalc.js
@@ -14,7 +14,7 @@ Note
 function fuelPrice(litres, pricePerLiter) {
   for (let i = 2; i <= 10; i +=2){
     if (litres >= i) {
-      pricePerLiter -= 0.05
+      pricePerLiter -= 0.05;
     }
   }
   return Math.round(litres * pricePerLiter * 100) / 100
