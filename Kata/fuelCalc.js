91f0e4b916b335c13e59518b2bae553a5537e97e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-06-19 10:01:59 -0400

    Update fuelCalc.js

--- Kata/fuelCalc.js
+++ Kata/fuelCalc.js
@@ -10,3 +10,12 @@ Good Luck!
 Note
 1 Dollar = 100 Cents
 */
+
+function fuelPrice(litres, pricePerLiter) {
+  for (let i = 2; i <= 10; i +=2){
+    if (litres >= i) {
+      pricePerLiter -= 0.05
+    }
+  }
+  return Math.round(litres * pricePerLiter * 100) / 100
+}
