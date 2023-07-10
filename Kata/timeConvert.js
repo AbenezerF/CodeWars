cd7531ecfbc00bba5de28297420d264cfdbd466b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-10 14:29:27 -0400

    Update timeConvert.js

--- Kata/timeConvert.js
+++ Kata/timeConvert.js
@@ -13,8 +13,8 @@ Good luck! :D
 */
 
 function timeConvert(num) { 
-let val = (Math.floor(num / 60)).toString()
-let secondVal = (num % 60).toString()
-if(num < 0) return "00:00"
-return val.padStart(2, "0") + ":" + secondVal.padStart(2,"0")
+  let val = (Math.floor(num / 60)).toString()
+  let secondVal = (num % 60).toString()
+  if(num < 0) return "00:00"
+  return val.padStart(2, "0") + ":" + secondVal.padStart(2,"0")
 }
