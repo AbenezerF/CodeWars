b5970cdc2876fb7581bde1f9f28191a014c8b9a8
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-11 10:24:33 -0400

    Update keypadHorror.js

--- Kata/keypadHorror.js
+++ Kata/keypadHorror.js
@@ -28,7 +28,7 @@ function computerToPhone(numbers){
   let num = numbers.split('').map( x => Number(x))
   for(let i = 0; i < num.length; i++){
     if(num[i] > 6) fin.push(num[i] - 6);
-    else if(num[i] < 4 && num[i] >= 1) fin.push(num[i] + 6)
+    else if(num[i] < 4 && num[i] >= 1) fin.push(num[i] + 6);
     else fin.push(num[i])
   }
   return fin.join('');
