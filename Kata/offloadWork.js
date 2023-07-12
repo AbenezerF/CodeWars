18c705349468db7aaf08abf9281e2908f296d12b
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-12 08:02:09 -0400

    Update offloadWork.js

--- Kata/offloadWork.js
+++ Kata/offloadWork.js
@@ -1,1 +1,9 @@
 // Offload your work! (7Kyu)
+
+
+function workNeeded(projectMinutes, freelancers){
+  let countMins = freelancers.reduce((acc,[hour, minutes]) => acc + hour * 60 + minutes, 0)
+  let remainingHours = Math.floor((projectMinutes - countMins) / 60)
+  let remainingMin = (projectMinutes - countMins) % 60
+  return projectMinutes - countMins <= 0 ? "Easy Money!" : `I need to work ${remainingHours} hour(s) and ${remainingMin} minute(s)`
+}
