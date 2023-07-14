7bc2afff27663585574462593a2d380235ff2bd8
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-14 11:07:34 -0400

    Update identicalElem.js

--- Kata/identicalElem.js
+++ Kata/identicalElem.js
@@ -5,3 +5,10 @@ Given two arrays of integers m and n, test if they contain at least one identica
 
 Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
 */
+
+function duplicateElements(m, n) {
+  for(let i = 0; i < m.length; i++){
+    if(n.some( (x) => x == m[i])) return true
+  }
+  return false
+}
