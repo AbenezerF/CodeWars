38e9f75e194b83e1daf773bc88b992679ab5c5f9
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-17 08:56:43 -0400

    Update meanMedian.js

--- Kata/meanMedian.js
+++ Kata/meanMedian.js
@@ -10,3 +10,12 @@ Reminder: Median
 
 Array will always be valid (odd-length >= 3)
 */
+
+function meanVsMedian(numbers) {
+  let avg = numbers.reduce( (curr, prev) => curr + prev, 0) / numbers.length
+  let sorted = numbers.sort( (a,b) => a - b)
+  if(avg  > sorted[Math.floor(numbers.length/2)]) return 'mean' 
+  else if(avg  < sorted[Math.floor(numbers.length/2)]) return 'median' 
+  else return 'same'
+
+}
