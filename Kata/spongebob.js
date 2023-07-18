06d56aeaefa71bdb91d42c790215b21f6e6c06d8
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-18 07:01:58 -0400

    Update spongebob.js

--- Kata/spongebob.js
+++ Kata/spongebob.js
@@ -15,7 +15,7 @@ function spongeMeme(sentence) {
   let arr = sentence.split('');
   for(let i = 0; i < arr.length; i++){
     if(i % 2 == 0) arr[i] = arr[i].toUpperCase();
-    else arr[i] = arr[i].toLowerCase()
+    else arr[i] = arr[i].toLowerCase();
   }
   return arr.join('')
 }
