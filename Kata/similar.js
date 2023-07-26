5d1a0c73c7ebb70d2351ef777f0a9ab31b20a276
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-07-26 10:38:48 -0400

    Update similar.js

--- Kata/similar.js
+++ Kata/similar.js
@@ -30,3 +30,10 @@ Constraints: B.length = A.length, 1 ≤ B[i] ≤ 1000.
 
 true if A and B are similar, false otherwise.
 */
+
+function areSimilar(A, B) {
+  var s = A.map((x,i) => [x, i]).filter(x => x[0] !== B[x[1]]);
+  if (s.length == 0) return true;
+  if (s.length != 2) return false;
+  return s[0][0] === B[s[1][1]] && s[1][0] === B[s[0][1]];
+}
