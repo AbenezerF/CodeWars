3468e4172f96749383378f651439d9944b94de07
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-01 07:04:44 -0400

    Update reverseTry.js

--- Kata/reverseTry.js
+++ Kata/reverseTry.js
@@ -25,7 +25,7 @@ function firstReverseTry(arr) {
   if (arr.length > 1) {
     let first = arr[0]; 
     arr[0] = arr[arr.length-1];
-    arr[arr.length-1] = first;
+    arr[arr.length-1] = first
   }
   return arr;
 }
