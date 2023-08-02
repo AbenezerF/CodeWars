152788dfc5049b0f91dcc48e1ac83085979c3943
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-02 09:00:54 -0400

    Update missingElem.js

--- Kata/missingElem.js
+++ Kata/missingElem.js
@@ -8,3 +8,12 @@ Please note, there may be duplicates in the arrays, so checking if a numerical v
 find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
 find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
 */
+
+function findMissing(arr1, arr2) {
+  arr1 = arr1.sort( (a,b) => a - b)
+  arr2 = arr2.sort( (a,b) => a - b)
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] != arr2[i]) return arr1[i]
+  }
+  return 0;
+}
