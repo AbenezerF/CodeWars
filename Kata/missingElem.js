ac8822ebe864d17efd1613e80375440d8c9a3b62
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-02 09:01:26 -0400

    Update missingElem.js

--- Kata/missingElem.js
+++ Kata/missingElem.js
@@ -13,7 +13,7 @@ function findMissing(arr1, arr2) {
   arr1 = arr1.sort( (a,b) => a - b);
   arr2 = arr2.sort( (a,b) => a - b);
   for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] != arr2[i]) return arr1[i]
+    if(arr1[i] != arr2[i]) return arr1[i];
   }
   return 0;
 }
