1773b4a6100c9dd4d226e7b88553b385f5a3a368
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-04 07:58:59 -0400

    Add Pull your words together, man! Kata

--- Kata/words.js
+++ Kata/words.js
@@ -1,12 +1,1 @@
-// Words to sentence (7Kyu)
-/*
-Write function which will create a string from a list of strings, separated by space.
-
-Example:
-
-["hello", "world"] -> "hello world"
-*/
-
-function wordsToSentence(words) {
-  return words.join(' ');
-}
+// Pull your words together, man!
