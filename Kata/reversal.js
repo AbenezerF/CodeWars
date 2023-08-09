9f458d4f4f77c684d7585dda27167da90e6a9f3c
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-09 18:01:01 -0400

    Update reversal.js

--- Kata/reversal.js
+++ Kata/reversal.js
@@ -29,5 +29,5 @@ Sorted array a with all the trees untouched.
 
 function sortByHeight(a) {
   const sorted = a.filter(v => v !== -1).sort((a, b) => b-a);
-  return a.map(v => v == -1 ? -1 : sorted.pop())
+  return a.map(v => v == -1 ? -1 : sorted.pop());
 }
