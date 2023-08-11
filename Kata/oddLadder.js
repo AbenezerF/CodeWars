e19fce3edbc63c8f26199b3beb3bdfb0e96be5f0
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-11 08:38:28 -0400

    Update oddLadder.js

--- Kata/oddLadder.js
+++ Kata/oddLadder.js
@@ -1,1 +1,29 @@
 // Complete The Pattern #6 - Odd Ladder (7Kyu)
+
+/*
+###Task:
+
+You have to write a function pattern which creates the following pattern (see examples) up to the desired number of rows.
+
+If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+
+If any even number is passed as argument then the pattern should last upto the largest odd number which is smaller than the passed even number.
+
+###Examples:
+
+pattern(9):
+
+1
+333
+55555
+7777777
+999999999
+pattern(6):
+
+1
+333
+55555
+Note: There are no spaces in the pattern
+
+Hint: Use \n in string to jump to next line
+*/
