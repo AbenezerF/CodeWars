c1c220d0f5bc69cbceb1d5dd5ba8ef9d772fd5ae
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-19 10:50:06 -0400

    Update calcType.js

--- Kata/calcType.js
+++ Kata/calcType.js
@@ -13,7 +13,7 @@ calcType(1, 2, 3) -->   1 ? 2 = 3   --> "addition"
 
 function calcType(a, b, res) {
   if(a + b == res) return "addition";
-  else if(a - b == res) return "subtraction"
+  else if(a - b == res) return "subtraction";
   else if(a * b == res) return "multiplication"
   else return "division"
 }
