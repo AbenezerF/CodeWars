33171830de0c80af9b93baea0a0583996835fcaa
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-19 10:49:45 -0400

    Update calcType.js

--- Kata/calcType.js
+++ Kata/calcType.js
@@ -1,1 +1,12 @@
 // Find the calculation type (7Kyu)
+
+/*
+You have to create a function calcType, which receives 3 arguments: 2 numbers, and the result of an unknown operation performed on them (also a number).
+
+Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+
+The possible return strings are: "addition", "subtraction", "multiplication", "division".
+
+Example:
+calcType(1, 2, 3) -->   1 ? 2 = 3   --> "addition"
+*/
