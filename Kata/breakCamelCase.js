c61c066113bde2e52bc24176b9941ae70761cc9e
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-22 11:19:56 -0400

    Update breakCamelCase.js

--- Kata/breakCamelCase.js
+++ Kata/breakCamelCase.js
@@ -8,3 +8,10 @@ Example
 "identifier"   =>  "identifier"
 ""             =>  ""
 */
+
+function solution(string) {
+  let newStr = string[0].toUpperCase() + string.slice(1)
+  let arr = newStr.match(/[A-Z][a-z]+/g)
+  let final = arr.join(" ")
+  return final[0].toLowerCase() + final.slice(1)
+}
