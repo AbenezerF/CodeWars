e919557f89e6204974c31afda5872e3a946c62be
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-22 11:19:46 -0400

    Update breakCamelCase.js

--- Kata/breakCamelCase.js
+++ Kata/breakCamelCase.js
@@ -1,1 +1,10 @@
 // Break camelCase (6Kyu)
+
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
