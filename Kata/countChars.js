23209e7d88f86933a733b354e20293178ce0c4ad
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-08-23 08:12:16 -0400

    Update countChars.js

--- Kata/countChars.js
+++ Kata/countChars.js
@@ -5,3 +5,12 @@ The main idea is to count all the occurring characters in a string. If you have 
 
 What if the string is empty? Then the result should be empty object literal, {}.
 */
+
+function count (string) { 
+  let obj = {}
+  for(let i = 0; i < string.length; i++){
+    if(obj[string[i]] >= 1) obj[string[i]]++
+    else obj[string[i]] = 1
+  }
+  return obj 
+}
