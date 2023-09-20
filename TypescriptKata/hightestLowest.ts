40218dae10234c68b0171c45dd2478d5b44800db
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-09-20 14:08:16 -0400

    Update hightestLowest.ts

--- TypescriptKata/hightestLowest.ts
+++ TypescriptKata/hightestLowest.ts
@@ -15,6 +15,6 @@ Output string must be two numbers separated by a single space, and highest numbe
 export class Kata {
   static highAndLow(numbers: string): string {
   let arr = numbers.split(' ').map(x => Number(x))
-    return Math.max(...arr).toString() + " " + Math.min(...arr).toString();
+    return Math.max(...arr).toString() + " " + Math.min(...arr).toString()
   }
 }
