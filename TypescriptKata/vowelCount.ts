365356ed3f5f508ecbdd8c4976bf48608d1486ae
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-10-23 08:29:55 -0400

    Update vowelCount.ts

--- TypescriptKata/vowelCount.ts
+++ TypescriptKata/vowelCount.ts
@@ -7,3 +7,14 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
 */
+
+export class Kata {
+  static getCount(str: string): number {
+    let total = 0
+    let v: String[] = ["a", "e", "i", "o", "u"]
+    for(let i = 0; i < str.length; i++){
+      if(v.includes(str[i])) total++;
+    }
+    return total
+  }
+}
