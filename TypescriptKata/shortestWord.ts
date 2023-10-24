1d900773f3f09fb55472bccc57aee73ac8896ae4
AbenezerF <77024369+AbenezerF@users.noreply.github.com>
2023-10-24 08:01:24 -0400

    Update shortestWord.ts

--- TypescriptKata/shortestWord.ts
+++ TypescriptKata/shortestWord.ts
@@ -4,3 +4,8 @@ Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
+export function findShort(s: string): number {
+  let arr: number[] = s.split(" ").map( (x) => x.length);
+  arr = arr.sort( (a,b) => a - b)
+  return arr[0]; // your code here
+}
